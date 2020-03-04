b6019d4a94113bb15e19fd91df9c2c6ff729f5d4
shenjinxiang <sjx-sword@163.com>
2020-03-04 17:46:11 +0800

    update server port

--- app.js
+++ app.js
@@ -6,7 +6,7 @@ const app = express();
 
 app.use(express.static(path.join(__dirname, 'public')));
 
-app.listen(6098, function() {
-    console.log('Server running at 6098!');
-    console.log('http://localhost:6098');
+app.listen(4001, function() {
+    console.log('Server running at 4001!');
+    console.log('http://localhost:4001');
 });
