ebe0e24728db376e5dc822482ec200bb9cd6cb32
shenjinxiang <sjx-sword@163.com>
2020-04-02 23:02:28 +0800

    排序动画15%

--- public/排序动画-冒泡排序/index.js
+++ public/排序动画-冒泡排序/index.js
@@ -22,10 +22,34 @@
             drawer.count = 0;
             drawer.plans = [];
             drawer.plans.push(new drawer.Plan(0));
-            drawer.plans.push(new drawer.Plan(1));
-            drawer.plans.push(new drawer.Plan(2));
-            drawer.plans.push(new drawer.Plan(3));
+            drawer.algorithm();
+            console.log(drawer.plans);
             drawer.currentPlan = drawer.plans.shift();
+            
+        },
+        algorithm: function() {
+            var arr = [];
+            drawer.elements.forEach(function(item) {
+                arr.push(item.val);
+            });
+            //console.log(drawer.elements);
+            //console.log(arr);
+            var len = arr.length;
+            for (var i = 0; i < len - 1; i++) {
+                for (var j = 0; j < len - 1 - i; j++) {
+                    if (arr[j] > arr[j+1]) {        // 相邻元素两两对比
+                        var temp = arr[j+1];        // 元素交换
+                        arr[j+1] = arr[j];
+                        arr[j] = temp;
+                        drawer.addGroupPlan(j, j + 1);
+                    }
+                }
+            }
+        },
+        addGroupPlan: function(index, jndex) {
+            drawer.plans.push(new drawer.Plan(1, index, jndex));
+            drawer.plans.push(new drawer.Plan(2, index, jndex));
+            drawer.plans.push(new drawer.Plan(3, index, jndex));
         },
         animate: function() {
             drawer.update();
@@ -33,8 +57,9 @@
             requestAnimationFrame(drawer.animate);
         },
         update: function() {
-            console.log(drawer.currentPlan);
-            console.log('status:', drawer.currentPlan['status'], 'cnt:', drawer.count);
+            //console.log(drawer.currentPlan);
+            //console.log('status:', drawer.currentPlan['status'], 'cnt:', drawer.count);
+            drawer.currentPlan.update();
             drawer.count++;
             if (drawer.count >= drawer.currentPlan.count) {
                 drawer.currentPlan = drawer.plans.shift();
@@ -48,9 +73,7 @@
             var ctx = drawer.ctx;
             ctx.fillStyle = drawer.option.background;
             ctx.fillRect(0, 0, drawer.w, drawer.h);
-            drawer.elements.forEach(function(item) {
-                item.draw(ctx, drawer.currentStatus);
-            });
+            drawer.currentPlan.draw();
         },
         initElements: function() {
             var nums = drawer.option.nums,
@@ -77,17 +100,20 @@
             this.sx = drawer.getXByIndex(this.index);
             this.sy = drawer.c.height / 2;
             this.color = color;
+            this.active = false;
             this.update = function() {
             };
             this.draw = function(ctx, currentStatus) {
-                if (currentStatus == 0) {
-                    ctx.save();
-                    ctx.fillStyle = this.color;
-                    ctx.beginPath();
+                ctx.save();
+                ctx.fillStyle = this.color;
+                ctx.beginPath();
+                if (!this.active) {
                     ctx.arc(this.sx, this.sy, this.r, 0, 2 * Math.PI, false);
-                    ctx.fill();
-                    ctx.restore();
+                } else {
+                    ctx.arc(this.ax, this.ay, this.r, 0, 2 * Math.PI, false);
                 }
+                ctx.fill();
+                ctx.restore();
             }
             function val2r(val) {
                 return val;
@@ -99,11 +125,29 @@
                 w = drawer.w;
             return 0.5 * w - 0.5 * (num -1) * space + index * space;
         },
-        Plan: function(planStatus, ele1, ele2) {
+        Plan: function(planStatus, index, jndex) {
+            var thisPlan = this;
             this['status'] = planStatus;
-            this.ele1 = ele1;
-            this.ele2 = ele2;
+            this.i = index;
+            this.j = jndex;
             this.count = drawer.option.counts[planStatus];
+            this.update = function() {
+                drawer.elements.forEach(function(item, index, arr) {
+                    item.active = (index == thisPlan.i || index == thisPlan.j);
+                });
+            };
+            this.draw = function() {
+                drawer.elements.forEach(function(item) {
+                    if (!item.active) {
+                        item.draw(drawer.ctx, thisPlan['status']);
+                    } 
+                });
+                var ix = drawer.elements[this.i].x,
+                    iy = drawer.elements[this.i].y,
+                    jx = drawer.elements[this.j].x,
+                    jy = drawer.elements[this.j].y;
+
+            }
         }
 
     };
