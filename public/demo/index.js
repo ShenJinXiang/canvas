9dd5c92d01cb65ed48396c56a91772864e2c0f7a
shenjinxiang <sjx-sword@163.com>
2020-04-20 06:14:51 +0800

    添加八卦图

--- public/demo/index.js
+++ public/demo/index.js
@@ -1,33 +1,33 @@
 (function() {
-    /*
-     * 桃心型线的参数方程：
-     * x = 16 （sinθ）^3
-     * y = 13 cosθ- 5 cos 2θ - 2 cos 3θ - cos 4θ
-     * 玫瑰线的参数方程：
-     * x=sin4θ×cosθ
-     * y=sin4θ×sinθ
-     */
-
     let drawer = {
         option: {
-            radius: 0.4,
-            num: 100,
-            colors: [ '#0f628b', '#ccdff0', '#66ebff', '#ffffff', '#f0ff00' ],
-            rectElementWidth: 8,
-            maxElementRadius: 6,
-            minElementRadius: 0.5,
-            minRoundRadius: 4,
-            maxRoundRadius: 8,
-            minRoundTime: 50,
-            maxRoundTime: 100,
+            darkColor: '#000',
+            lightColor: '#fff',
+            innerTimes: 200,
+            outerTimes: 1000,
+            innerRadius: 0.25,
+            innerPRadius: 0.25 / 3,
+            rectWidth: 0.25 / 3,
+            rectHeight: 0.25 / 15,
+            outerRadius: 0.3,
+            outerTextSize: 0.05,
+            outerArr: [
+                {txt: '坎', val: '010', type: 1},
+                {txt: '艮', val: '001', type: 0},
+                {txt: '坤', val: '000', type: 0},
+                {txt: '震', val: '100', type: 0},
+                {txt: '离', val: '101', type: 0},
+                {txt: '兑', val: '110', type: 1},
+                {txt: '乾', val: '111', type: 1},
+                {txt: '巽', val: '011', type: 1}
+            ]
         },
         init: function() {
             this.c = document.getElementById("canvas");
-            this.w = this.c.width = window.innerWidth;
-            this.h = this.c.height = window.innerHeight;
-            this.outer = Math.min(this.w, this.h);
             this.ctx = this.c.getContext('2d');
-            this.initElements();
+            this.innerRorate = this.outerRorate = 0;
+            this.innerStep = 2 * Math.PI / this.option.innerTimes;
+            this.outerStep = 2 * Math.PI / this.option.outerTimes;
             this.animate();
         },
         animate: function() {
@@ -39,101 +39,105 @@
             drawer.w = drawer.c.width = window.innerWidth;
             drawer.h = drawer.c.height = window.innerHeight;
             drawer.outer = Math.min(drawer.w, drawer.h);
-            drawer.refreshElements();
-            drawer.elments.forEach(item => item.update());
+            drawer.innerRorate += drawer.innerStep;
+            drawer.outerRorate -= drawer.outerStep;
         },
         draw: function() {
             let ctx = drawer.ctx;
-            ctx.clearRect(0, 0, drawer.w, drawer.h);
             ctx.save();
             ctx.translate(drawer.w / 2, drawer.h / 2);
-            drawer.elments.forEach(item => item.draw(ctx));
+            drawer.drawInner();
+            drawer.drawerOuter();
             ctx.restore();
-        },
-        initElements: function() {
-            drawer.elments = [];
-            let step = 2 * Math.PI / drawer.option.num,
-                r = drawer.outer * drawer.option.radius,
-                color = drawer.option.colors;
-            for (let i = 0; i < drawer.option.num; i++) {
-                let p = drawer.heartXY(i * step, r / 16),
-                    c = color[Math.floor(Math.random() * color.length)],
-                    roundRadius = drawer.random(drawer.option.minRoundRadius, drawer.option.maxRoundRadius),
-                    roundTime = drawer.random(drawer.option.minRoundTime, drawer.option.maxRoundTime);
-                if (Math.random() > 0.6) {
-                    drawer.elments.push(new drawer.RectElement(p.x, p.y, c, roundRadius, roundTime));
-                } else {
-                    drawer.elments.push(new drawer.CircleElement(p.x, p.y, c, roundRadius, roundTime));
-                }
-            }
-        },
-        refreshElements: function() {
-            let step = 2 * Math.PI / drawer.option.num,
-                r = drawer.outer * drawer.option.radius;
-            drawer.elments.forEach(function(item, i) {
-                let p = drawer.heartXY(i * step, r / 16);
-                item.x = p.x;
-                item.y = p.y;
-            });
 
         },
-        CircleElement: function(x, y, color, roundRadius, roundTime) {
-            this.x = x;
-            this.y = y;
-            this.color = color;
-            this.roundRadius = roundRadius;
-            this.roundStep = 2 * Math.PI / roundTime;
-            this.rotate = 0;
-            this.r = drawer.random(drawer.option.minElementRadius, drawer.option.maxElementRadius);
-            this.update = function() {
-                this.rotate += this.roundStep;
-            },
-            this.draw = function(ctx) {
+        drawInner: function() {
+            let ctx = drawer.ctx,
+                rotate = drawer.innerRorate,
+                outer = drawer.outer,
+                radius = drawer.option.innerRadius * outer,
+                pRadius = drawer.option.innerPRadius * outer,
+                darkColor = drawer.option.darkColor,
+                lightColor = drawer.option.lightColor;
+            ctx.save();
+            ctx.rotate(rotate);
+
+            ctx.beginPath();
+            ctx.fillStyle = lightColor;
+            ctx.arc(0, 0, radius, 0, Math.PI, true);
+            ctx.arc(-radius / 2, 0, radius / 2, Math.PI, 0, false);
+            ctx.arc(radius / 2, 0, radius / 2, Math.PI, 0, true);
+            ctx.fill();
+
+            ctx.beginPath();
+            ctx.fillStyle = darkColor;
+            ctx.arc(0, 0, radius, 0, Math.PI, false);
+            ctx.arc(-radius / 2, 0, radius / 2, Math.PI, 0, false);
+            ctx.arc(radius / 2, 0, radius / 2, Math.PI, 0, true);
+            ctx.fill();
+
+            ctx.beginPath();
+            ctx.fillStyle = lightColor;
+            ctx.arc(-radius / 2, 0, pRadius / 2, 0, 2 * Math.PI, false);
+            ctx.fill();
+
+            ctx.beginPath();
+            ctx.fillStyle = darkColor;
+            ctx.arc(radius / 2, 0, pRadius / 2, 0, 2 * Math.PI, false);
+            ctx.fill();
+
+            ctx.restore();
+        },
+        drawerOuter: function() {
+            debugger
+            let ctx = drawer.ctx,
+                rotate = drawer.outerRorate,
+                outer = drawer.outer,
+                radius = drawer.option.outerRadius * outer,
+                darkColor = drawer.option.darkColor,
+                lightColor = drawer.option.lightColor,
+                rectWidth = drawer.option.rectWidth * outer,
+                rectHeight = drawer.option.rectHeight * outer,
+                arr = drawer.option.outerArr,
+                outerTextSize = drawer.option.outerTextSize * outer,
+                step = Math.PI / 4;
+
+            arr.forEach(function(item, index) {
                 ctx.save();
-                ctx.translate(this.x, this.y);
-                ctx.fillStyle = this.color;
-                ctx.beginPath();
-                ctx.arc(this.roundRadius * Math.cos(this.rotate), this.roundRadius * Math.sin(this.rotate), this.r, 0, 2 * Math.PI, false);
-                ctx.fill();
-                ctx.restore();
-            }
-        },
-        RectElement: function(x, y, color, roundRadius, roundTime) {
-            this.x = x;
-            this.y = y;
-            this.color = color;
-            this.roundRadius = roundRadius;
-            this.roundStep = 2 * Math.PI / roundTime;
-            this.rotate = 0;
-            this.w = drawer.option.rectElementWidth;
-            this.update = function() {
-                this.rotate += this.roundStep;
-            },
-            this.draw = function(ctx) {
+                ctx.rotate(rotate + step * index);
+                let valArr = item.val.split('');
+                valArr.forEach(function(val, index) {
+                    let x = radius + 2 * index * rectHeight;
+                    if (val == '1') {
+                        ctx.beginPath();
+                        ctx.fillStyle = lightColor;
+                        ctx.fillRect(x, -rectWidth / 2, rectHeight, rectWidth);
+                    } else if (val == '0') {
+                        ctx.beginPath();
+                        ctx.fillStyle = darkColor;
+                        ctx.fillRect(x, -rectWidth / 2, rectHeight, 0.4 * rectWidth);
+
+                        ctx.beginPath();
+                        ctx.fillStyle = darkColor;
+                        ctx.fillRect(x, 0.1 * rectWidth, rectHeight, 0.4 * rectWidth);
+                    }
+                });
+
                 ctx.save();
-                ctx.translate(this.x, this.y);
-                ctx.rotate(Math.PI / 4);
-                ctx.fillStyle = this.color;
+                ctx.rotate(-Math.PI / 2);
                 ctx.beginPath();
-                ctx.fillRect(this.roundRadius * Math.cos(this.rotate) - this.w / 2, this.roundRadius * Math.sin(this.rotate) - this.w / 2, this.w, this.w);
+                ctx.fillStyle = item.type == 1 ? '#fff' : '#000';
+                ctx.font = outerTextSize + 'px cursive';
+                ctx.textAlign = 'center';
+                ctx.textBaseline = 'top';
+                ctx.fillText(item.txt, 0, radius + 6 * rectHeight);
+
                 ctx.restore();
-            }
-        },
-        heartXY: function (θ, r) {
-            return {
-                x: r * 16 * Math.pow(Math.sin(θ), 3),
-                y: -r * (13 * Math.cos(θ)- 5 * Math.cos(2 * θ) - 2 * Math.cos(3 * θ) - Math.cos(4 * θ) )
-            };
-        },
-        mgXY: function (t, r) {
-            return {
-                x : r * (Math.sin(4 * t) * Math.cos(t)),
-                y : r * (Math.sin(4 * t) * Math.sin(t))
-            };
-        },
-        random: function(min, max) {
-            return Math.random() * (max - min) + min;
+                ctx.restore();
+            });
+
         }
+
     };
     drawer.init();
 })();
