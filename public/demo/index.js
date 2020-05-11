192221595c1518a157b6f5e9d7c75f247948a351
shenjinxiang <sjx-sword@163.com>
2020-05-11 15:17:12 +0800

    开始分形图五

--- public/demo/index.js
+++ public/demo/index.js
@@ -1,3 +1,109 @@
 (() => {
+    const option = {
+        deepNum: 4,
+        timeStep: 100,
+        color: '#0075c9',
+    };
+
+    class Element {
+        constructor(sx, sy, ex, ey, len, angle, color, time, move, tx, ty) {
+            this.sx = sx;
+            this.sy = sy;
+            this.ex = ex;
+            this.ey = ey;
+            this.len = len;
+            this.angle = angle;
+            this.color = color;
+            this.time = time;
+            this.move = move;
+            if (this.move) {
+                this.tx = tx;
+                this.ty = ty;
+                this.xStep = (this.ex - this.tx) / this.time;
+                this.yStep = (this.ey - this.ty) / this.time;
+            }
+            this.start();
+
+            this.mx = this.sx + (this.ex - this.sx) / 2;
+            this.my = this.sy + (this.ey - this.sy) / 2;
+            this.clen = this.len * Math.sin(Math.PI / 4);
+            this.sAngle = this.angle + Math.PI / 4;
+            this.eAngle = -this.angle - Math.PI / 4;
+            this.mex = this.sx + this.clen * Math.cos(this.sAngle);
+            this.mey = this.sy + this.clen * Math.sin(this.sAngle);
+        }
+        start() {
+            this.current = 0;
+            if(this.move) {
+                this.dx = this.tx;
+                this.dy = this.ty;
+            } else {
+                this.dx = this.ex;
+                this.dy = this.ey;
+            }
+        }
+        complete() {
+            this.current = this.time;
+            this.dx = this.ex;
+            this.dy = this.ey;
+        }
+        update() {
+            if (this.current < this.time) {
+                this.current++;
+                if (this.move) {
+                    this.dx = this.tx + this.current * this.xStep;
+                    this.dy = this.ty + this.current + this.yStep;
+                }
+            }
+        }
+        draw(ctx) {
+            ctx.save();
+            ctx.strokeStyle = this.color;
+            ctx.beginPath();
+            ctx.moveTo(this.sx, this.sy);
+            ctx.lineTo(this.dx, this.dy);
+            ctx.stroke();
+            ctx.restore();
+        }
+        children() {
+            return [
+                new Element(this.sx, this.sy, this.mex, this.mey, this.clen, this.sAngle, this.color, this.time, true, this.mx, this.my),
+                new Element(this.ex, this.ey, this.mex, this.mey, this.clen, this.eAngle, this.color, this.time, true, this.mx, this.my),
+            ];
+        }
+    }
+    const drawer = {
+        start() {
+            drawer.c = document.getElementById('canvas');
+            drawer.w = drawer.c.width = window.innerWidth;
+            drawer.h = drawer.c.height = window.innerHeight;
+            drawer.ctx = drawer.c.getContext('2d');
+            drawer.len = Math.min(drawer.w, drawer.h) * 0.6;
+            drawer.initElementGroup();
+            console.log(drawer.elements);
+        },
+        initElementGroup() {
+            drawer.elements = [];
+            for (let i = 0; i < option.deepNum; i++) {
+                let eles = [];
+                if (i === 0) {
+                    eles.push(new Element(
+                        (drawer.w - drawer.len) / 2,
+                        drawer.h / 2,
+                        (drawer.w + drawer.len) / 2,
+                        drawer.h / 2,
+                        drawer.len,
+                        0,
+                        option.color,
+                        option.timeStep,
+                        false
+                    ));
+                } else {
+                    drawer.elements[i - 1].forEach(item => eles.push(eles = eles.concat(item.children())));
+                }
+                drawer.elements.push(eles);
+            }
+        }
+    };
     drawer.start();
 })();
