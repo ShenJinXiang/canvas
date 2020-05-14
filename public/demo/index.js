5e210332df4b922ec3739c852de63d99ae63d437
shenjinxiang <sjx-sword@163.com>
2020-05-14 23:00:25 +0800

    多球体旋转练习

--- public/demo/index.js
+++ public/demo/index.js
@@ -1,23 +1,98 @@
 (() => {
     const option = {
-        deepNum: 6,
-        timeStep: 20,
-        sleepTime: 20,
-        color: '#0075c9',
+        num: 32,
+        radius: 100,
+        sRadius: 200,
+        sColor: 'rgba(255, 255, 255, 0.5)',
+        ballRadius: 10,
+        ballColor: '#084',
+        angleStep: Math.PI / 90,
     };
+
     class Element {
-        constructor(sx, sy, width, angle, color, time, move, counterclockwise) {
-            this.sx = sx;
-            this.sy = sy;
-            this.width = width;
-            this.angle = angle;
+        constructor(x, y, radius, ballRadius, beginAngle, angleStep, color, ballColor, counterclockwise) {
+            this.x = x;
+            this.y = y;
+            this.radius = radius;
+            this.ballRadius = ballRadius;
+            this.beginAngle = beginAngle;
+            this.angleStep = angleStep;
             this.color = color;
-            this.time = time;
-            this.move = move;
+            this.ballColor = ballColor;
             this.counterclockwise = counterclockwise;
+
+            this.currentAngle = beginAngle;
+        }
+        update() {
+            if(this.counterclockwise) {
+                this.currentAngle += this.angleStep;
+            } else {
+                this.currentAngle -= this.angleStep;
+            }
         }
         draw(ctx) {
-            
+            ctx.save();
+            ctx.translate(this.x, this.y);
+
+            ctx.beginPath();
+            ctx.strokeStyle = this.color;
+            ctx.arc(0, 0, this.radius, 0, 2 * Math.PI, false);
+            ctx.stroke();
+
+            ctx.beginPath();
+            ctx.fillStyle = this.ballColor;
+            ctx.arc(
+                this.radius * Math.cos(this.currentAngle),
+                this.radius * Math.sin(this.currentAngle),
+                this.ballRadius,
+                0,
+                2 * Math.PI,
+                false);
+            ctx.fill();
+            ctx.restore();
         }
     }
+
+    const drawer = {
+        start() {
+            drawer.c = document.getElementById('canvas');
+            drawer.w = drawer.c.width = window.innerWidth;
+            drawer.h = drawer.c.height = window.innerHeight;
+            drawer.ctx = drawer.c.getContext('2d');
+            drawer.initElements();
+            console.log(this.elements);
+            drawer.animate();
+        },
+        animate() {
+            drawer.update();
+            drawer.draw();
+            requestAnimationFrame(drawer.animate);
+        },
+        update() {
+            drawer.elements.forEach(item => item.update());
+        },
+        draw() {
+            let ctx = drawer.ctx;
+            ctx.clearRect(0, 0, drawer.w, drawer.h);
+            drawer.elements.forEach(item => item.draw(ctx));
+        },
+        initElements() {
+            drawer.elements = [];
+            let step = 2 * Math.PI / option.num;
+            for (let i = 0; i < option.num; i++) {
+                drawer.elements.push(new Element(
+                    drawer.w / 2 + option.radius * Math.cos(i * step),
+                    drawer.h / 2 + option.radius * Math.sin(i * step),
+                    option.sRadius,
+                    option.ballRadius,
+                     - i * step,
+                    option.angleStep,
+                    option.sColor,
+                    option.ballColor,
+                    false
+                ));
+            }
+        }
+    }
+    drawer.start();
 })();
---
+++ public/demo/index_bck.js
@@ -0,0 +1,23 @@
+(() => {
+    const option = {
+        deepNum: 6,
+        timeStep: 20,
+        sleepTime: 20,
+        color: '#0075c9',
+    };
+    class Element {
+        constructor(sx, sy, width, angle, color, time, move, counterclockwise) {
+            this.sx = sx;
+            this.sy = sy;
+            this.width = width;
+            this.angle = angle;
+            this.color = color;
+            this.time = time;
+            this.move = move;
+            this.counterclockwise = counterclockwise;
+        }
+        draw(ctx) {
+
+        }
+    }
+})();
