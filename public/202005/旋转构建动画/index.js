adb27157fbdd63ff44e09d4c24671d761c70813f
shenjinxiang <sjx-sword@163.com>
2020-05-15 17:28:06 +0800

    添加旋转构建动态图

---
+++ public/202005/旋转构建动画/index.js
@@ -0,0 +1,145 @@
+(() => {
+    const option = {
+        num: 18,
+        radius: 100,
+        sRadius: 220,
+        sColor: 'rgba(255, 255, 255, 0.5)',
+        ballRadius: 10,
+        ballColor: '#084',
+        angleStep: Math.PI / 90,
+    };
+
+    class Element {
+        constructor(x, y, radius, ballRadius, beginAngle, angleStep, color, ballColor, counterclockwise) {
+            this.x = x;
+            this.y = y;
+            this.radius = radius;
+            this.ballRadius = ballRadius;
+            this.beginAngle = beginAngle;
+            this.angleStep = angleStep;
+            this.color = color;
+            this.ballColor = ballColor;
+            this.counterclockwise = counterclockwise;
+
+            this.currentAngle = beginAngle;
+        }
+        refreshXY(x, y, radius, ballRadius) {
+            this.x = x;
+            this.y = y;
+            this.radius = radius;
+            this.ballRadius = ballRadius;
+        }
+        update() {
+            if(this.counterclockwise) {
+                this.currentAngle += this.angleStep;
+            } else {
+                this.currentAngle -= this.angleStep;
+            }
+        }
+        draw(ctx) {
+            ctx.save();
+            ctx.translate(this.x, this.y);
+
+            ctx.beginPath();
+            ctx.strokeStyle = this.color;
+            ctx.arc(0, 0, this.radius, 0, 2 * Math.PI, false);
+            ctx.stroke();
+
+            ctx.beginPath();
+            ctx.fillStyle = this.ballColor;
+            ctx.arc(
+                this.radius * Math.cos(this.currentAngle),
+                this.radius * Math.sin(this.currentAngle),
+                this.ballRadius,
+                0,
+                2 * Math.PI,
+                false);
+            ctx.fill();
+            ctx.restore();
+        }
+    }
+
+    const drawer = {
+        start() {
+            drawer.c = document.getElementById('canvas');
+            drawer.range = document.getElementById("numRange");
+            drawer.init();
+            drawer.ctx = drawer.c.getContext('2d');
+            drawer.initElements();
+            drawer.animate();
+            drawer.bindEvent();
+        },
+        init() {
+            drawer.w = drawer.c.width = window.innerWidth;
+            drawer.h = drawer.c.height = window.innerHeight;
+            drawer.mark = CanvasUtil.getMarkCanvas('#999');
+            drawer.grid = Math.min(drawer.w, drawer.h);
+            option.radius = drawer.grid * 0.15;
+            option.sRadius = drawer.grid * 0.30;
+            option.ballRadius = drawer.grid * 0.01;
+        },
+        bindEvent() {
+            window.onresize = drawer.reset;
+            $("#numRange").mousemove(function() {
+                let val = ~~$(this).val();
+                $("#num_span").text(val);
+                if (option.num !== val) {
+                    option.num = val;
+                    drawer.initElements();
+                }
+            });
+
+        },
+        reset() {
+            drawer.init();
+            drawer.refreshElements();
+        },
+        animate() {
+            drawer.update();
+            drawer.draw();
+            requestAnimationFrame(drawer.animate);
+        },
+        update() {
+            drawer.elements.forEach(item => item.update());
+        },
+        draw() {
+            let ctx = drawer.ctx;
+            ctx.clearRect(0, 0, drawer.w, drawer.h);
+            drawer.elements.forEach(item => item.draw(ctx));
+            CanvasUtil.drawMark(ctx, drawer.mark);
+        },
+        refreshElements() {
+            let step = 2 * Math.PI / option.num;
+            drawer.elements.forEach((item, index) => {
+                item.refreshXY(
+                    drawer.w / 2 + option.radius * Math.cos(index * step),
+                    drawer.h / 2 + option.radius * Math.sin(index * step),
+                    option.sRadius,
+                    option.ballRadius
+                )
+            });
+        },
+        initElements() {
+            drawer.elements = [];
+            let step = 2 * Math.PI / option.num;
+            for (let i = 0; i < option.num; i++) {
+                let c = i * 10;
+                drawer.elements.push(new Element(
+                    drawer.w / 2 + option.radius * Math.cos(i * step),
+                    drawer.h / 2 + option.radius * Math.sin(i * step),
+                    option.sRadius,
+                    option.ballRadius,
+                     - i * step,
+                    option.angleStep,
+                    option.sColor,
+                    // option.ballColor,
+                    'hsla(' + c  + ', 80%, 60%, 1)',
+                    false
+                ));
+            }
+        }
+    };
+
+    drawer.start();
+    drawer.bindEvent();
+})();
--- public/demo/index.js
+++ public/demo/index.js
@@ -1,98 +1,26 @@
 (() => {
     const option = {
-        num: 32,
-        radius: 100,
-        sRadius: 200,
         sColor: 'rgba(255, 255, 255, 0.5)',
         ballRadius: 10,
         ballColor: '#084',
         angleStep: Math.PI / 90,
     };
-
-    class Element {
-        constructor(x, y, radius, ballRadius, beginAngle, angleStep, color, ballColor, counterclockwise) {
-            this.x = x;
-            this.y = y;
-            this.radius = radius;
-            this.ballRadius = ballRadius;
-            this.beginAngle = beginAngle;
-            this.angleStep = angleStep;
-            this.color = color;
-            this.ballColor = ballColor;
-            this.counterclockwise = counterclockwise;
-
-            this.currentAngle = beginAngle;
-        }
-        update() {
-            if(this.counterclockwise) {
-                this.currentAngle += this.angleStep;
-            } else {
-                this.currentAngle -= this.angleStep;
-            }
-        }
-        draw(ctx) {
-            ctx.save();
-            ctx.translate(this.x, this.y);
-
-            ctx.beginPath();
-            ctx.strokeStyle = this.color;
-            ctx.arc(0, 0, this.radius, 0, 2 * Math.PI, false);
-            ctx.stroke();
-
-            ctx.beginPath();
-            ctx.fillStyle = this.ballColor;
-            ctx.arc(
-                this.radius * Math.cos(this.currentAngle),
-                this.radius * Math.sin(this.currentAngle),
-                this.ballRadius,
-                0,
-                2 * Math.PI,
-                false);
-            ctx.fill();
-            ctx.restore();
-        }
-    }
-
     const drawer = {
         start() {
             drawer.c = document.getElementById('canvas');
+            drawer.ctx = drawer.c.getContext('2d');
+            drawer.mark = CanvasUtil.getMarkCanvas('#999');
+
             drawer.w = drawer.c.width = window.innerWidth;
             drawer.h = drawer.c.height = window.innerHeight;
-            drawer.ctx = drawer.c.getContext('2d');
-            drawer.initElements();
-            console.log(this.elements);
-            drawer.animate();
+            drawer.width = Math.min(drawer.w, drawer.h);
+            drawer.radius = 0.45 * drawer.width;
+            drawer.ballRadius = 0.01 * drawer.width;
+
+
         },
-        animate() {
-            drawer.update();
-            drawer.draw();
-            requestAnimationFrame(drawer.animate);
-        },
-        update() {
-            drawer.elements.forEach(item => item.update());
-        },
-        draw() {
-            let ctx = drawer.ctx;
-            ctx.clearRect(0, 0, drawer.w, drawer.h);
-            drawer.elements.forEach(item => item.draw(ctx));
-        },
-        initElements() {
-            drawer.elements = [];
-            let step = 2 * Math.PI / option.num;
-            for (let i = 0; i < option.num; i++) {
-                drawer.elements.push(new Element(
-                    drawer.w / 2 + option.radius * Math.cos(i * step),
-                    drawer.h / 2 + option.radius * Math.sin(i * step),
-                    option.sRadius,
-                    option.ballRadius,
-                     - i * step,
-                    option.angleStep,
-                    option.sColor,
-                    option.ballColor,
-                    false
-                ));
-            }
-        }
-    }
+    };
+
     drawer.start();
+    drawer.bindEvent();
 })();
