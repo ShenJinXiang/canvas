d0ffb07b70ceade7384e9938facb6a91d528bc33
shenjinxiang <sjx-sword@163.com>
2020-06-02 22:02:47 +0800

    完成心形波函数

--- public/demo/index.js
+++ public/demo/index.js
@@ -1,9 +1,12 @@
 (() => {
     /*
     b = -13;
-    y = Math.pow(x, 2 / 3) + 0.9 * Math.sqrt(20.3 - Math.pow(x , 2)) - Math.sin(5 * b * Math.PI * x);
+    y = Math.pow(x * x, 1 / 3) + 0.9 * Math.sqrt(20.3 - Math.pow(x , 2)) - Math.sin(5 * b * Math.PI * x);
 
     x=[-2:.001:2];y=(sqrt(cos(x)).*cos(200*x)+sqrt(abs(x))-0.7).*(4-x.*x).^0.01;plot(x,y);
+
+    POWER(A3^2,1/3)+0.9*SQRT(3.3-A3^2)*SIN($G$2*PI()*A3)
+    Math.power(x * x, 1 / 3) + 0.9 * Math.sqrt(3.3 - x * x) * Math.sin( * Math.PI * x)
 
     range           x
     ----   =   --------------
@@ -20,7 +23,7 @@
         coordinateArrowLength: 25,
         strokeStyle: '#F00',
         strokeWidth: 2,
-        range: [-5, 5],
+        range: [-10, 5],
     };
     class Point {
         constructor(x, y) {
@@ -29,13 +32,14 @@
         }
     }
     class Element {
-        constructor(minX, maxX, xStep, func, strokeWidth, strokecolor) {
+        constructor(minX, maxX, xStep, func, strokeWidth, strokecolor, scale) {
             this.minX = minX;
             this.maxX = maxX;
             this.xStep = xStep;
             this.func = func;
             this.strokeWidth = strokeWidth;
             this.strokecolor = strokecolor;
+            this.scale = scale;
             this.points = [];
             this.createPoints();
         }
@@ -50,7 +54,7 @@
             ctx.strokeStyle = this.strokecolor;
             ctx.beginPath();
             this.points.forEach((item) => {
-                ctx.lineTo(item.x, item.y);
+                ctx.lineTo(item.x * this.scale, -item.y * this.scale * 0.8);
             });
             ctx.stroke();
             ctx.restore();
@@ -63,15 +67,84 @@
             drawer.ctx = drawer.c.getContext('2d');
             drawer.mark = CanvasUtil.getMarkCanvas();
             drawer.reset();
+            drawer.initElements();
+            drawer.current = 0;
+            drawer.animate();
+            drawer.bindEvent();
         },
         reset() {
             drawer.w = drawer.c.width = window.innerWidth;
             drawer.h = drawer.c.height = window.innerHeight;
-            drawer.origin = new Point(drawer.w / 2, drawer.h / 2);
+            drawer.origin = new Point(drawer.w / 2, drawer.h * 0.6);
             drawer.range = 10;
             drawer.width = drawer.w - option.padding - option.padding;
             drawer.step = drawer.width / drawer.range;
+        },
+        bindEvent() {
+            window.onresize = drawer.reset;
+        },
+        animate() {
+            drawer.update();
+            drawer.draw();
+            requestAnimationFrame(drawer.animate);
+        },
+        update() {
+            drawer.current++;
+            if (drawer.current >= drawer.elements.length) {
+                drawer.current = 0;
+            }
+            console.log(drawer.current);
+        },
+        initElements() {
+            drawer.elements = [];
+            for (let b = -30; b < 30; b+=0.05) {
+                drawer.elements.push(new Element(-3, 3, 0.01, (x) => {
+                    return Math.pow(x * x, 1 / 3) + 0.9 * Math.sqrt(3.3 - x * x) * Math.sin(b * Math.PI * x);
+                }, option.strokeWidth, option.strokeStyle, drawer.step));
+            }
+        },
+        draw() {
+            let ctx = drawer.ctx;
+            ctx.clearRect(0, 0, drawer.w, drawer.h);
+            drawer.drawCoordinate(ctx);
+            ctx.save();
+            ctx.translate(drawer.origin.x, drawer.origin.y);
+            drawer.elements[drawer.current].draw(ctx);
+            ctx.restore();
+            CanvasUtil.drawMark(ctx, drawer.mark);
+        },
+        drawCoordinate(ctx) {
+            ctx.save();
+            ctx.strokeStyle = option.coordinateColor;
+            ctx.lineWidth = option.coordinateLineWidth;
+
+            ctx.beginPath();
+            ctx.moveTo(option.padding, drawer.origin.y);
+            ctx.lineTo(drawer.w - option.padding - option.coordinateArrowLength, drawer.origin.y);
+            ctx.stroke();
+
+            ctx.beginPath();
+            ctx.moveTo(drawer.origin.x, option.padding + option.coordinateArrowLength);
+            ctx.lineTo(drawer.origin.x, drawer.h - option.padding);
+            ctx.stroke();
+
+            ctx.fillStyle = option.coordinateColor;
+            ctx.beginPath();
+            ctx.moveTo(drawer.w - option.padding, drawer.origin.y);
+            ctx.lineTo(drawer.w - option.padding - option.coordinateArrowLength, drawer.origin.y - option.coordinateArrowWidth / 2);
+            ctx.lineTo(drawer.w - option.padding - option.coordinateArrowLength, drawer.origin.y + option.coordinateArrowWidth / 2);
+            ctx.closePath();
+            ctx.fill();
+
+            ctx.beginPath();
+            ctx.moveTo(drawer.origin.x, option.padding);
+            ctx.lineTo(drawer.origin.x - option.coordinateArrowWidth / 2, option.padding + option.coordinateArrowLength);
+            ctx.lineTo(drawer.origin.x + option.coordinateArrowWidth / 2, option.padding + option.coordinateArrowLength);
+            ctx.closePath();
+            ctx.fill();
+            ctx.restore();
         }
 
     };
+    drawer.start();
 })();
