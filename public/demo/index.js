bcfd22b23c76ca9adb1594826ac2f180c3060656
shenjinxiang <sjx-sword@163.com>
2020-06-24 22:49:25 +0800

    code

--- public/demo/index.js
+++ public/demo/index.js
@@ -2,6 +2,8 @@
     const option = {
         lineWidth: 1,
         lineLength: 10,
+        num: 3,
+        times: [300, 400],
 
     };
     class Element {
@@ -18,9 +20,10 @@
                 y: this.start.y + this.length * Math.sin(this.angle)
             };
         }
-        draw(ctx) {
+        draw(ctx, origin) {
             ctx.save();
-            ctx.translate(this.point.x, this.point.y);
+            origin = origin ? origin: {x: 0, y: 0};
+            ctx.translate(this.point.x - origin.x, this.point.y - origin.y);
             ctx.strokeStyle = this.style;
             ctx.lineWidth = this.width;
             ctx.beginPath();
@@ -37,6 +40,46 @@
         }
     }
 
+    class ElementGroup {
+        constructor(elemnts, time1, time2) {
+            this.elements = elemnts;
+            this.time1 = time1;
+            this.time2 = time2;
+            this.start = this.elements[0].start;
+            this.end = this.elements[this.elements.length - 1].end;
+            this.status = 0;
+
+            this.angleStep = Math.PI / 2 / this.time2;
+            this.time1Count = 0;
+            this.time2Count = 0;
+        }
+        update() {
+            if (this.status === 0) {
+                this.time1Count++;
+                if (this.time1Count >= this.time1) {
+                    this.start = 1;
+                }
+            }
+            if (this.status === 1) {
+                this.angle = this.angleStep * this.time2Count;
+                this.time2Count++;
+                if (this.time2Count >= this.time2) {
+                    this.status = 2;
+                }
+            }
+        }
+        draw(ctx) {
+            this.elements.forEach((item) => {
+                item.draw(ctx);
+            });
+            if (this.status === 1) {
+                ctx.save();
+                ctx.restore();
+            }
+        }
+
+    }
+
     const drawer = {
         start() {
             drawer.c = document.getElementById('canvas');
@@ -50,7 +93,10 @@
 
         },
         initElements() {
-            
+            drawer.elements = [];
+            for (let i = 0; i < option.num; i++) {
+
+            }
         }
     };
     drawer.start();
