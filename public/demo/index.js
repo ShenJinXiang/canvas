c8dc40f622d1b166deb09ffe2b134cfe4dc8785d
shenjinxiang <sjx-sword@163.com>
2020-06-30 22:39:26 +0800

    新demo

--- public/demo/index.js
+++ public/demo/index.js
@@ -1,3 +1,23 @@
 {
 
+    const drawer = {
+        start() {
+            drawer.c = document.getElementById('canvas');
+            drawer.ctx = drawer.c.getContext('2d');
+            drawer.init();
+            drawer.draw();
+        },
+        init() {
+            drawer.w = drawer.c.width = window.innerWidth;
+            drawer.h = drawer.c.height = window.innerHeight;
+            drawer.ox = drawer.w / 2;
+            drawer.oy = drawer.y / 2;
+        },
+        draw() {
+            let ctx = drawer.ctx;
+            ctx.save();
+            ctx.translate(drawer.ox, drawer.oy);
+            ctx.restore();
+        }
+    };
 }
