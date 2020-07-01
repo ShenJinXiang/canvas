7f411cda4d1e569ca742882d945c486abbf85378
shenjinxiang <sjx-sword@163.com>
2020-07-01 10:38:03 +0800

    螺旋线

--- public/demo/index.js
+++ public/demo/index.js
@@ -1,17 +1,61 @@
 {
+    /**
+     * dAngle = Math.PI * 2;
+     * dLength = width * 8;
+     * al = Math.PI / (width * 4)
+     */
 
+    class Element {
+        constructor(ox, oy, beginLength, beginAngle, lengthStep, angleStep, width, style) {
+            this.ox = ox;
+            this.oy = oy;
+            this.beginLength = beginLength;
+            this.beginAngle = beginAngle;
+            this.lengthStep = lengthStep;
+            this.angleStep = angleStep;
+            this.width = width;
+            this.style = style;
+        }
+        draw(ctx, maxLength) {
+            let len = this.beginLength,
+                angle = this.beginAngle;
+            ctx.save();
+            ctx.translate(this.ox, this.oy);
+            ctx.strokeStyle = this.style;
+            ctx.lineWidth = this.width;
+            ctx.beginPath();
+            while (angle <= maxLength) {
+                ctx.lineTo(len * Math.cos(angle), len * Math.sin(angle));
+                len += this.lengthStep;
+                angle += this.angleStep;
+            }
+            ctx.stroke();
+            ctx.restore();
+        }
+    }
     const drawer = {
         start() {
             drawer.c = document.getElementById('canvas');
             drawer.ctx = drawer.c.getContext('2d');
             drawer.init();
-            drawer.draw();
+            // drawer.draw();
+            let ele = new Element(
+                drawer.ox,
+                drawer.oy,
+                20,
+                0,
+                0.1,
+                Math.PI / 400,
+                10,
+                'red'
+            );
+            ele.draw(drawer.ctx, Math.max(drawer.ox, drawer.oy));
         },
         init() {
             drawer.w = drawer.c.width = window.innerWidth;
             drawer.h = drawer.c.height = window.innerHeight;
             drawer.ox = drawer.w / 2;
-            drawer.oy = drawer.y / 2;
+            drawer.oy = drawer.h / 2;
         },
         draw() {
             let ctx = drawer.ctx;
@@ -20,4 +64,5 @@
             ctx.restore();
         }
     };
+    drawer.start();
 }
