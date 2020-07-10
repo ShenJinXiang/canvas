dfd95bdb0d8e4d38f6c17922eaeb96545a78364d
shenjinxiang <sjx-sword@163.com>
2020-07-11 07:20:33 +0800

    gridline

--- public/demo/index.js
+++ public/demo/index.js
@@ -12,16 +12,30 @@
         },
         init() {
             drawer.width = Math.min(window.innerWidth, window.innerHeight);
-            drawer.c.width = drawer.c.height = drawer.width;
-            drawer.gridWidth = drawer.width * 0.8;
+            drawer.w = drawer.c.width = window.innerWidth;
+            drawer.h = drawer.c.height = window.innerHeight;
+            drawer.gridWidth = drawer.width * 0.9;
+            drawer.half = drawer.gridWidth / 2;
         },
         drawGridLine(ctx) {
             ctx.save();
-            ctx.translate(drawer.width / 2, drawer.width / 2);
+            ctx.translate(drawer.w / 2, drawer.h / 2);
             ctx.strokeStyle = option.lineColor;
-            ctx.lineWidth = 2;
-            ctx.strokeRect(-drawer.gridWidth / 2, -drawer.gridWidth / 2, drawer.gridWidth, drawer.gridWidth);
+            ctx.lineWidth = 3;
+            ctx.strokeRect(-drawer.half, -drawer.half, drawer.gridWidth, drawer.gridWidth);
+
+            ctx.setLineDash([2, 5, 2]);
+            CanvasUtil.line(ctx, 0, -drawer.half, 0, drawer.half, option.lineColor, 1);
+            CanvasUtil.line(ctx, -drawer.half, 0, drawer.half, 0, option.lineColor, 1);
+            CanvasUtil.line(ctx, -drawer.half, -drawer.half, drawer.half, drawer.half, option.lineColor, 1);
+            CanvasUtil.line(ctx, drawer.half, -drawer.half, -drawer.half, drawer.half, option.lineColor, 1);
             ctx.restore();
+        },
+        bindEvent() {
+            drawer.c.addEventListener('mousemove', drawer.move, false);
+        },
+        move(e) {
+
         }
     };
     drawer.start();
