9c95b77bbccaebf31f51020325686f27735a7aee
shenjinxiang <sjx-sword@163.com>
2020-07-10 22:18:47 +0800

    d

--- public/demo/index.js
+++ public/demo/index.js
@@ -1,3 +1,28 @@
 {
-    
+    const option = {
+        lineColor: 'hsla(0, 100%, 60%, 1)'
+    }
+
+    const drawer = {
+        start() {
+            drawer.c = document.getElementById('canvas');
+            drawer.ctx = drawer.c.getContext('2d');
+            drawer.init();
+            drawer.drawGridLine(drawer.ctx);
+        },
+        init() {
+            drawer.width = Math.min(window.innerWidth, window.innerHeight);
+            drawer.c.width = drawer.c.height = drawer.width;
+            drawer.gridWidth = drawer.width * 0.8;
+        },
+        drawGridLine(ctx) {
+            ctx.save();
+            ctx.translate(drawer.width / 2, drawer.width / 2);
+            ctx.strokeStyle = option.lineColor;
+            ctx.lineWidth = 2;
+            ctx.strokeRect(-drawer.gridWidth / 2, -drawer.gridWidth / 2, drawer.gridWidth, drawer.gridWidth);
+            ctx.restore();
+        }
+    };
+    drawer.start();
 }
