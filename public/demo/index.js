eb33302aa778df3851483ca581c3d26efac2df56
shenjinxiang <sjx-sword@163.com>
2020-07-17 18:01:45 +0800

    df

--- public/demo/index.js
+++ public/demo/index.js
@@ -8,42 +8,36 @@
             this.angle = angle;
             this.rotate = rotate;
             this.style = style;
-
+            this.initPath();
+        }
+        initPath() {
             this.points = [
                 {x: 0, y: 0},
                 {x: this.width2 * Math.cos(this.angle), y: this.width2 * Math.sin(this.angle)},
                 {x: this.width2 * Math.cos(this.angle) + this.width1, y: this.width2 * Math.sin(this.angle)},
                 {x: this.width1, y: 0},
-            ]
-
+            ];
+            this.path = new Path2D();
+            this.points.forEach((item) => {
+                this.path.lineTo(item.x, item.y);
+            });
+            this.path.closePath();
         }
-        stroke(ctx, style) {
-            this.draw(ctx, 'stroke', style);
-        }
-        fill(ctx, style) {
-            this.draw(ctx, 'fill', style);
-        }
-        draw(ctx, type, style) {
+        stroke(ctx, style, lineWidth = 1) {
             ctx.save();
             ctx.translate(this.x, this.y);
             ctx.rotate(this.rotate);
-            if (type === 'stroke') {
-                ctx.strokeStyle = style || this.style;
-            }
-            if (type === 'fill') {
-                ctx.fillStyle = style || this.style;
-            }
-            ctx.beginPath();
-            this.points.forEach((item) => {
-                ctx.lineTo(item.x, item.y);
-            });
-            ctx.closePath();
-            if (type === 'stroke') {
-                ctx.stroke();
-            }
-            if (type === 'fill') {
-                ctx.fill();
-            }
+            ctx.strokeStyle = style || this.style;
+            ctx.lineWidth = lineWidth;
+            ctx.stroke(this.path);
+            ctx.restore();
+        }
+        fill(ctx, style) {
+            ctx.save();
+            ctx.translate(this.x, this.y);
+            ctx.rotate(this.rotate);
+            ctx.fillStyle = style || this.style;
+            ctx.fill(this.path);
             ctx.restore();
         }
     }
@@ -53,14 +47,17 @@
             drawer.c = document.getElementById('canvas');
             drawer.ctx = drawer.c.getContext('2d');
             drawer.init();
-
-            let ele = new Parallelogram(100, 100, 200, 150, Math.PI / 4, 0, 'red');
-            ele.stroke(drawer.ctx);
+            
+            let ele = new Parallelogram(100, 100, 300, 240, Math.PI / 4, 0, '#000');
             ele.fill(drawer.ctx, 'green');
+            ele.stroke(drawer.ctx, 'red', 3);
         },
         init() {
             drawer.w = drawer.c.width = window.innerWidth;
             drawer.h = drawer.c.height = window.innerHeight;
+            drawer.width = Math.min(drawer.w, drawer.h);
+
+            // initElements();
         }
 
     };
