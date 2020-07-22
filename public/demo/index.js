7f82268482d27c2541c4dbdfaa7fdbb0656f6dc8
shenjinxiang <sjx-sword@163.com>
2020-07-22 12:41:46 +0800

    完成旋转动画

--- public/demo/index.js
+++ public/demo/index.js
@@ -1,6 +1,8 @@
 {
     const option = {
-        time: 100
+        time: 600,
+        sideNum: 5,
+        num: 60,
     };
 
     class Polygon {
@@ -14,6 +16,15 @@
 
             this.angleStep = 2 * Math.PI / num;
         }
+        getRadius() {
+            return this.radius;
+        }
+        setRadius(value) {
+            this.radius = value;
+        }
+        setRotate(rotate) {
+            this.rotate = rotate;
+        }
         generatePath(radius) {
             let path = new Path2D();
             for (let i = 0; i < this.num; i++) {
@@ -25,33 +36,26 @@
             path.closePath();
             return path;
         }
-        stroke(ctx, lineWidth, radius, rotate, style) {
-            debugger
-            let _radius = radius || this.radius,
-                _rotate = rotate || this.rotate,
-                _style = style || this.style,
-                _lineWidth = lineWidth | 1,
-                path = this.generatePath(_radius);
+        stroke(ctx, lineWidth) {
+            let path = this.generatePath(this.radius);
             ctx.save();
             ctx.translate(this.ox, this.oy);
-            ctx.rotate(_rotate);
-            ctx.strokeStyle = _style;
-            ctx.lineWidth = _lineWidth;
+            ctx.rotate(this.rotate);
+            ctx.strokeStyle = this.style;
+            ctx.lineWidth = lineWidth || 1;
             ctx.stroke(path);
             ctx.restore();
         }
-        fill(ctx, radius, rotate, style) {
-            let _radius = radius || this.radius,
-                _rotate = rotate || this.rotate,
-                _style = style || this.style,
-                path = this.generatePath(_radius);
+        fill(ctx) {
+            let path = this.generatePath(this.radius);
             ctx.save();
             ctx.translate(this.ox, this.oy);
-            ctx.rotate(_rotate);
-            ctx.fillStyle = _style;
+            ctx.rotate(this.rotate);
+            ctx.fillStyle = this.style;
             ctx.fill(path);
             ctx.restore();
         }
+
     }
 
     const drawer = {
@@ -59,15 +63,64 @@
             drawer.c = document.getElementById('canvas');
             drawer.ctx = drawer.c.getContext('2d');
             drawer.init();
-            let ele = new Polygon(drawer.w / 2, drawer.h / 2, 5, 100, 0, 'red');
-            ele.stroke(drawer.ctx);
+            // let ele = new Polygon(drawer.w / 2, drawer.h / 2, 5, 100, 0, 'red');
+            // ele.stroke(drawer.ctx);
+            drawer.animate();
         },
         init() {
             drawer.w = drawer.c.width = window.innerWidth;
             drawer.h = drawer.c.height = window.innerHeight;
+            drawer.width = Math.min(drawer.w, drawer.h);
+            drawer.radius = drawer.width * 0.45;
+            drawer.ox = drawer.w / 2;
+            drawer.oy = drawer.h / 2;
+            drawer.initElements();
+            drawer.count = 0;
+            // drawer.angle = 0;
+            drawer.angleStep = 2 * Math.PI / option.sideNum / option.time;
+            drawer.sideAngle = (option.sideNum - 2) * Math.PI / option.sideNum / 2;
+        },
+        initElements() {
+            drawer.elements = [];
+            for (let i = 0; i < option.num; i++) {
+                drawer.elements.push(new Polygon(
+                    drawer.ox,
+                    drawer.oy,
+                    option.sideNum,
+                    drawer.radius,
+                    -Math.PI / 2,
+                    'hsla(' + (i * 15) + ', 100%, 60%, 1)'
+                ));
+            }
+        },
+        animate() {
+            drawer.update();
+            drawer.draw();
+            requestAnimationFrame(drawer.animate);
+        },
+        update() {
+            // console.log(drawer.count, drawer.elements[1].rotate, drawer.elements[1].radius);
+            let angle = drawer.count * drawer.angleStep,
+                bl = Math.sin(drawer.sideAngle) / Math.sin(Math.PI - drawer.sideAngle - angle);
+            drawer.elements.forEach((item, index) => {
+                item.setRotate(index * angle - Math.PI / 2);
+                if (index > 0) {
+                    item.setRadius(drawer.elements[index - 1].getRadius() * bl);
+                }
+            });
+            drawer.count++;
+            if (drawer.count > option.time) {
+                drawer.count = 0;
+            }
+
+        },
+        draw() {
+            let ctx = drawer.ctx;
+            ctx.clearRect(0, 0, drawer.w, drawer.h);
+            drawer.elements.map((item) => item.stroke(ctx));
         }
     };
 
-    // drawer.start();
+    drawer.start();
 
 }
