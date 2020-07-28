8ebfa7d3a7392d3b88878194e291809bf5d52280
shenjinxiang <sjx-sword@163.com>
2020-07-28 21:58:57 +0800

    基本完成彩虹糖跳动动画

--- public/demo/index.js
+++ public/demo/index.js
@@ -32,10 +32,72 @@
         start() {
             drawer.c = document.getElementById('canvas');
             drawer.ctx = drawer.c.getContext('2d');
+            drawer.mark = CanvasUtil.getMarkCanvas();
+            drawer.init();
+            console.log(drawer.elements);
+            drawer.animate();
         },
         init() {
             drawer.w = drawer.c.width = window.innerWidth;
             drawer.h = drawer.c.height = window.innerHeight;
+            drawer.radius = drawer.h * .35;
+            drawer.initElements();
+        },
+        animate() {
+            drawer.update();
+            drawer.draw();
+            requestAnimationFrame(drawer.animate);
+        },
+        update() {
+            drawer.elements.forEach((item) => item.update());
+        },
+        draw() {
+            let ctx = drawer.ctx;
+            ctx.clearRect(0, 0, drawer.w, drawer.h);
+            drawer.elements.forEach((item) => item.draw(ctx));
+            CanvasUtil.drawMark(ctx, drawer.mark);
+        },
+        initElements() {
+            drawer.elements = [];
+            let x = drawer.w / 2,
+                len = 0,
+                angle = 0,
+                count = 0,
+                cstep = 720 / (drawer.w / (option.radius * 2 + option.step));
+            console.log(cstep);
+            while(len < drawer.w / 2) {
+                if (len === 0) {
+                    drawer.elements.push(new Element(
+                        drawer.w / 2,
+                        drawer.h / 2,
+                        option.radius,
+                        drawer.radius,
+                        angle,
+                        'hsla(0, 100%, 60%, 1)'
+                    ));
+                } else {
+                    drawer.elements.push(new Element(
+                        drawer.w / 2 + len,
+                        drawer.h / 2,
+                        option.radius,
+                        drawer.radius,
+                        angle + count * 0.001,
+                        // 0,
+                        'hsla(' + (count * cstep) + ', 100%, 60%, 1)'
+                    ));
+                    drawer.elements.push(new Element(
+                        drawer.w / 2 - len,
+                        drawer.h / 2,
+                        option.radius,
+                        drawer.radius,
+                        angle - count * 0.001,
+                        // 0,
+                        'hsla(' + (count * cstep) + ', 100%, 60%, 1)'
+                    ));
+                }
+                len += 2 * option.radius + option.step;
+                count++;
+            }
         }
     };
 
