262410c9be6560b2298ab77290fb1557cf8ff65e
shenjinxiang <sjx-sword@163.com>
2020-07-30 20:59:48 +0800

    开始

--- public/demo/index.js
+++ public/demo/index.js
@@ -1,3 +1,16 @@
 {
+    const drawer = {
+        start() {
+            drawer.c = document.getElementById('canvas');
+            drawer.ctx = drawer.c.getContext('2d');
+            drawer.mark = CanvasUtil.getMarkCanvas();
+        },
+        init() {
+            drawer.w = drawer.c.width = window.innerWidth;
+            drawer.h = drawer.c.height = window.innerHeight;
+            drawer.radius = Math.min(drawer.w, drawer.h);
+        }
+    };
 
+    drawer.start();
 }
