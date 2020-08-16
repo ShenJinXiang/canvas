eb1920ff163c346dd477ec16155a239553ac20f2
shenjinxiang <sjx-sword@163.com>
2020-08-16 17:51:56 +0800

    d

--- public/demo/index.js
+++ public/demo/index.js
@@ -1,3 +1,13 @@
 {
-    
+    const option = {
+
+    };
+
+    class Element {
+        constructor(ox, oy, radius) {
+            this.ox = ox;
+            this.oy = oy;
+            this.radius = radius;
+        }
+    }
 }
