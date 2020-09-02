84f6857f01ad64109e8e6e0b406d3feb58ce1873
ShenJinXiang <sjx-sword@163.com>
2020-09-02 08:45:46 +0800

    fall

--- public/demo/index.js
+++ public/demo/index.js
@@ -1,19 +1,43 @@
 {
 
     class FireBall {
-        constructor(ox, oy, radius) {
+        constructor(ox, oy, radius, mx, my) {
+            this.sx = ox;
             this.ox = ox;
+            this.sy = oy;
             this.oy = oy;
             this.radius = radius;
+            this.mx = mx;
+            this.my = my;
             this.lineWidth = this.radius * 0.3;
 
             this.time = 0;
+            this.move = false;
+
+            this.vy = 1;
 
         }
         update() {
-            this.time += 1;
-            // this.color = 'hsl(' + Math.sin(this.time * Math.PI / 180) * 360 + ', 80%, 60%)';
-            this.color = 'hsl(' + this.time + ', 80%, 60%)';
+            if (!this.move) {
+                this.time += 1;
+                // this.color = 'hsl(' + Math.sin(this.time * Math.PI / 180) * 360 + ', 80%, 60%)';
+                this.color = 'hsl(' + this.time + ', 80%, 60%)';
+                if (this.time === 360) {
+                    this.move = true;
+                }
+            } else {
+                this.oy += this.vy;
+                if (this.oy >= this.my - this.radius) {
+                    this.oy = this.my;
+                    this.vy = -1;
+                }
+                if (this.oy <= this.sy) {
+                    this.oy = this.sy;
+                    this.vy = 1;
+                    this.move = false;
+                }
+            }
+            console.log(this.vy, this.oy);
         }
         draw(ctx) {
             this.drawLine(ctx);
@@ -24,8 +48,8 @@
             ctx.strokeStyle = this.color;
             ctx.lineWidth = this.lineWidth;
             ctx.beginPath();
-            ctx.moveTo(this.ox, 0);
-            ctx.lineTo(this.ox, this.oy);
+            ctx.moveTo(this.sx, 0);
+            ctx.lineTo(this.sx, this.sy);
             ctx.stroke();
             ctx.restore();
         }
@@ -50,10 +74,9 @@
             drawer.c = document.getElementById('canvas');
             drawer.ctx = drawer.c.getContext('2d');
             drawer.init();
-            drawer.ele = new FireBall(drawer.w / 2, drawer.h / 2, 10);
+            drawer.ele = new FireBall(drawer.w / 2, drawer.h / 2, 10, drawer.w, drawer.h);
+            console.log(drawer.ele);
             drawer.animate();
-            drawer.ele.update();
-            drawer.ele.draw(drawer.ctx);
         },
         init() {
             drawer.w = drawer.c.width = window.innerWidth;
