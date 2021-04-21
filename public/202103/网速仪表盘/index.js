8179dac6379244112fd277aa40f60b89d4d40ab5
shenjinxiang <sjx-sword@163.com>
2021-04-21 21:11:50 +0800

    完成静态显示

--- public/202103/网速仪表盘/index.js
+++ public/202103/网速仪表盘/index.js
@@ -23,11 +23,11 @@
             drawer.ox = drawer.w / 2;
             drawer.oy = drawer.h / 2;
             drawer.rs = [
-                drawer.r * 1.0,
-                drawer.r * 0.95,
-                drawer.r * 0.92,
-                drawer.r * 0.85,
-                drawer.r * 0.6
+                drawer.r * 1.0,  // 最外层半径(长刻度外半径)
+                drawer.r * 0.95, // 短刻度外半径
+                drawer.r * 0.92, // 刻度内半径
+                drawer.r * 0.85, // 刻度值位置半径
+                drawer.r * 0.6   // 红点所在圆半径
             ];
             drawer.vals = [];
             drawer.valStep = option.maxVal / option.stepNum;
@@ -35,15 +35,26 @@
                 drawer.vals.push({val: drawer.valStep * i, angle: i * option.angleRange / option.stepNum});
             }
             drawer.angleStep = option.angleRange / (option.stepNum * 5);
+            // 左侧0的起始角度
             drawer.startAngle = - (Math.PI / 2) - option.angleRange / 2;
 
+            // 红色校圆点的半径
             drawer.pointRadius = 4;
 
-            drawer.value = 20;
-            drawer.initByValue();
-        },
-        initByValue() {
+            // 文字"下载速度"相对于圆心所在位置Y值
+            drawer.xzsdY = -drawer.r * 0.23;
+            drawer.currentValueY = drawer.r * 0;
+            drawer.mbpsY = drawer.r * 0.23;
 
+            drawer.xzsdFontSize = drawer.r * .06
+            drawer.currentValueFontSize = drawer.r * 0.4;
+            drawer.mbpsFontSize = drawer.r * .06
+
+
+            // 当前值
+            drawer.currentValue = 40;
+            // 当前值对应的角度
+            drawer.currentAngle = option.angleRange * drawer.currentValue / option.maxVal;
         },
         drawStatic() {
             let ctx = drawer.ctx;
@@ -75,9 +86,19 @@
             ctx.rotate(-drawer.startAngle);
             // 刻度值
             drawer.vals.forEach(function (item) {
-                ctx.font = '12px cursive';
+                ctx.font = '12px Arial';
                 ctx.fillText(item.val, drawer.rs[3] * Math.cos(item.angle + drawer.startAngle), drawer.rs[3] * Math.sin(item.angle + drawer.startAngle));
             });
+
+            // 下载速度
+            ctx.beginPath();
+            ctx.font = drawer.xzsdFontSize + 'px Arial';
+            ctx.fillText('下载速度', 0, drawer.xzsdY);
+
+            // Mbps
+            ctx.beginPath();
+            ctx.font = drawer.mbpsFontSize + 'px Arial';
+            ctx.fillText('Mbps', 0, drawer.mbpsY);
             ctx.rotate(drawer.startAngle);
 
             // 圆弧
@@ -85,12 +106,25 @@
             ctx.arc(0, 0, drawer.rs[4], 0, option.angleRange, false);
             ctx.stroke();
 
+
+            ctx.restore();
+        },
+        drawValues() {
+            let ctx = drawer.ctx;
+            ctx.save();
+            ctx.translate(drawer.ox, drawer.oy);
+            ctx.rotate(drawer.startAngle);
+            ctx.strokeStyle = '#e1e1e1';
+            ctx.fillStyle = '#e1e1e1';
+            ctx.textAlign = 'center';
+            ctx.textBaseline = 'middle';
+
             // 圆弧上的圆点刻度
             ctx.beginPath();
             ctx.fillStyle = 'red';
             ctx.arc(
-                drawer.rs[4] * Math.cos(0),
-                drawer.rs[4] * Math.sin(0),
+                drawer.rs[4] * Math.cos(drawer.currentAngle),
+                drawer.rs[4] * Math.sin(drawer.currentAngle),
                 drawer.pointRadius,
                 0,
                 2 * Math.PI,
@@ -98,10 +132,13 @@
             );
             ctx.fill();
 
-
-            ctx.restore();
-        },
-        drawValues() {
+            ctx.rotate(-drawer.startAngle);
+            // 当前值
+            ctx.beginPath();
+            ctx.fillStyle = '#fff';
+            ctx.font = drawer.currentValueFontSize + 'px Arial';
+            ctx.fillText(drawer.currentValue.toFixed(1), 0, drawer.currentValueY);
+            ctx.rotate(drawer.startAngle);
 
             ctx.restore();
         }
