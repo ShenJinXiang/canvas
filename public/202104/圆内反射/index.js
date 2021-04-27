158e4e46db2c1c3cbc9926c986ad10b12e738d59
shenjinxiang <sjx-sword@163.com>
2021-04-27 20:52:21 +0800

    start

--- public/202104/圆内反射/index.js
+++ public/202104/圆内反射/index.js
@@ -4,6 +4,30 @@
     };
 
     const drawer = {
-
+        start() {
+            drawer.c = document.getElementById('canvas');
+            drawer.ctx = drawer.c.getContext('2d');
+            drawer.mark = CanvasUtil.getMarkCanvas('#999');
+            drawer.init();
+            drawer.draw();
+        },
+        init() {
+            drawer.w = drawer.c.width = window.innerWidth;
+            drawer.h = drawer.c.height = window.innerHeight;
+            drawer.radius = Math.min(drawer.w, drawer.h) * option.radius;
+        },
+        draw() {
+            let ctx = drawer.ctx;
+            ctx.clearRect(0, 0, drawer.w, drawer.h);
+            ctx.save();
+            ctx.translate(drawer.w / 2, drawer.h / 2);
+            ctx.beginPath();
+            ctx.strokeStyle = '#fff';
+            ctx.arc(0, 0, drawer.radius, 0, 2 * Math.PI, false);
+            ctx.stroke();
+            ctx.restore();
+        }
     };
+
+    drawer.start();
 }
