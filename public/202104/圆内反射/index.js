0cd8bb822b366b4a19a43c998aa81f757e072c39
shenjinxiang <sjx-sword@163.com>
2021-04-30 22:23:11 +0800

    基本结构

--- public/202104/圆内反射/index.js
+++ public/202104/圆内反射/index.js
@@ -1,27 +1,41 @@
 {
     const option = {
         radius: 0.4,
-        pointsLength: 400
+        pointsLength: 400,
+        speed: 10
     };
 
     class Point {
-        constructor(x, y, color) {
+        constructor(x, y, hue) {
             this.x = x;
             this.y = y;
-            this.color = color;
+            this.hue = hue;
+            this.color = "hsl(" + this.hue + ", 100%, 50%)";
         }
     }
 
     class PointPath {
-        constructor(startPoint, angle) {
+        constructor(startPoint, angle, speed, radius) {
             this.points = [startPoint];
             this.angle = angle;
+            this.speed = speed;
+            this.speedX = this.speed * Math.cos(angle);
+            this.speedY = this.speed * Math.sin(angle);
+            this.radius = this.radius;
+            this.rl = this.radius * this.radius;
         }
         addPoint(point) {
             this.points.push(point);
             if (this.points.length > option.pointsLength) {
                 this.points.shift();
             }
+        }
+        update() {
+            let last = this.points[this.points.length - 1],
+                next = new Point(last.x + this.speedX, last.y + this.speedY, last.hue + 1),
+                nr = next.x * next.x + next.y * next.y;
+
+
         }
         draw(ctx) {
             if (this.points.length <= 1) {
@@ -46,45 +60,22 @@
             drawer.mark = CanvasUtil.getMarkCanvas('#999');
             drawer.init();
             console.log(drawer);
+            drawer.animate();
             drawer.draw();
         },
         init() {
             drawer.w = drawer.c.width = window.innerWidth;
             drawer.h = drawer.c.height = window.innerHeight;
             drawer.radius = Math.min(drawer.w, drawer.h) * option.radius;
-            drawer.path = new PointPath(drawer.randomStartPoint(), 0);
-            drawer.path.addPoint(new Point(1, 1, drawer.color(1)));
-            drawer.path.addPoint(new Point(10, 2, drawer.color(20)));
-            drawer.path.addPoint(new Point(20, 3, drawer.color(30)));
-            drawer.path.addPoint(new Point(30, 4, drawer.color(40)));
-            drawer.path.addPoint(new Point(40, 5, drawer.color(50)));
-            drawer.path.addPoint(new Point(50, 6, drawer.color(60)));
-            drawer.path.addPoint(new Point(60, 7, drawer.color(70)));
-            drawer.path.addPoint(new Point(70, 8, drawer.color(80)));
-            drawer.path.addPoint(new Point(80, 9, drawer.color(90)));
-            drawer.path.addPoint(new Point(90, 10, drawer.color(100)));
-            drawer.path.addPoint(new Point(100, 11, drawer.color(110)));
-            drawer.path.addPoint(new Point(110, 12, drawer.color(120)));
-            drawer.path.addPoint(new Point(120, 13, drawer.color(130)));
-            drawer.path.addPoint(new Point(130, 14, drawer.color(140)));
-            drawer.path.addPoint(new Point(140, 15, drawer.color(150)));
-            drawer.path.addPoint(new Point(150, 16, drawer.color(160)));
-            drawer.path.addPoint(new Point(160, 17, drawer.color(170)));
-            drawer.path.addPoint(new Point(170, 18, drawer.color(180)));
-            drawer.path.addPoint(new Point(180, 19, drawer.color(190)));
-            drawer.path.addPoint(new Point(190, 20, drawer.color(200)));
-            drawer.path.addPoint(new Point(200, 21, drawer.color(210)));
+            drawer.path = new PointPath(drawer.randomStartPoint(), random(2 * Math.PI));
         },
-        randomStartPoint() {
-            let r = random(drawer.radius * .4, drawer.radius * .7),
-                angle = random(2 * Math.PI),
-                x = r * Math.cos(angle),
-                y = r * Math.sin(angle),
-                color = drawer.color(1);
-            return new Point(x, y, color);
+        animate() {
+            drawer.update();
+            drawer.draw();
+            requestAnimationFrame(drawer.animate);
         },
-        color(hue) {
-            return "hsl(" + hue + ", 100%, 50%)";
+        update() {
+            drawer.path.update();
         },
         draw() {
             let ctx = drawer.ctx;
@@ -97,6 +88,17 @@
             ctx.stroke();
             drawer.path.draw(ctx);
             ctx.restore();
+        },
+        randomStartPoint() {
+            let r = random(drawer.radius * .4, drawer.radius * .7),
+                angle = random(2 * Math.PI),
+                x = r * Math.cos(angle),
+                y = r * Math.sin(angle),
+                color = drawer.color(1);
+            return new Point(x, y, color);
+        },
+        color(hue) {
+            return "hsl(" + hue + ", 100%, 50%)";
         }
     };
 
