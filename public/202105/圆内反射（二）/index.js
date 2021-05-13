01eba727283d006aa4ed9121c1359c2899a9959c
ShenJinXiang <sjx-sword@163.com>
2021-05-13 09:01:24 +0800

    园内反射（二）开始编码

---
+++ public/202105/圆内反射（二）/index.js
@@ -0,0 +1,59 @@
+{
+    const option = {
+        innerRadius: 0.42,
+        outerRadius: 0.4,
+        speed: 10,
+        backgroundColor: '#000',
+        outerColor: '#ccc',
+        innerColor: '#999'
+    };
+
+    const drawer = {
+        start() {
+            drawer.c = document.getElementById('canvas');
+            drawer.ctx = document.c.getContext('2d');
+            drawer.mark = CanvasUtil.getMarkCanvas('#999');
+            drawer.init();
+            drawer.animate();
+        },
+        init() {
+            drawer.w = drawer.c.width = window.innerWidth;
+            drawer.h = drawer.c.height = window.innerHeight;
+            drawer.width = Math.min(drawer.w, drawer.h);
+            drawer.innerRadius = drawer.width * option.innerRadius;
+            drawer.outerRadius = drawer.width * option.outerRadius;
+        },
+        animate() {
+            drawer.update();
+            drawer.draw();
+        },
+        update() {
+
+        },
+        draw() {
+            let ctx = drawer.ctx;
+            drawer.drawStatic(ctx);
+
+            CanvasUtil.drawMark(ctx, drawer.mark);
+        },
+        drawStatic(ctx) {
+            ctx.clearRect(0, 0, drawer.w, drawer.h);
+            ctx.fillStyle = option.backgroundColor;
+            ctx.fillRect(0, 0, drawer.w, drawer.h);
+
+            ctx.save();
+            ctx.translate(drawer.w / 2, drawer.h / 2);
+            ctx.beginPath();
+            ctx.fillStyle = option.outerColor;
+            ctx.arc(0, 0, drawer.outerRadius, 0, 2 * Math.PI, false);
+            ctx.arc(0, 0, drawer.innerRadius, 0, 2 * Math.PI, true);
+            ctx.fill();
+
+            ctx.beginPath();
+            ctx.fillStyle = option.innerColor;
+            ctx.arc(0, 0, drawer.innerRadius, 0, 2 * Math.PI, false);
+            ctx.fill();
+            ctx.restore();
+        }
+    }
+}
