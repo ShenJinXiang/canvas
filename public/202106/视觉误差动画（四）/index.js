ed529752a366c372db95f2618813120204231554
ShenJinXiang <sjx-sword@163.com>
2021-06-01 08:43:16 +0800

    代码框架

--- public/202106/视觉误差动画（四）/index.js
+++ public/202106/视觉误差动画（四）/index.js
@@ -2,4 +2,30 @@
     const option = {
         
     };
+    const drawer = {
+        start() {
+            drawer.c = document.getElementById('canvas');
+            drawer.ctx = drawer.c.getContext('2d');
+            drawer.mark = CanvasUtil.getMarkCanvas('#fff');
+            drawer.current = 0;
+            drawer.init();
+            drawer.animate();
+        },
+        init() {
+
+        },
+        animate() {
+            drawer.draw();
+            drawer.update();
+            requestAnimationFrame(drawer.animate);
+        },
+        draw() {
+
+        },
+        update() {
+
+        }
+    };
+
+    drawer.start();
 }
