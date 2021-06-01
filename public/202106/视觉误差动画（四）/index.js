ed8363d207653f713630e036fe9e084e062b96c2
ShenJinXiang <sjx-sword@163.com>
2021-06-01 18:05:11 +0800

    s

--- public/202106/视觉误差动画（四）/index.js
+++ public/202106/视觉误差动画（四）/index.js
@@ -1,17 +1,22 @@
 {
     const option = {
-        
+        backgroundColor: '#000',
+        eleWidth: 0.05,
+
     };
     const drawer = {
         start() {
             drawer.c = document.getElementById('canvas');
             drawer.ctx = drawer.c.getContext('2d');
             drawer.mark = CanvasUtil.getMarkCanvas('#fff');
-            drawer.current = 0;
             drawer.init();
             drawer.animate();
         },
         init() {
+            drawer.w = drawer.c.width = window.innerWidth;
+            drawer.h = drawer.c.height = window.innerHeight;
+            drawer.width = Math.min(drawer.w, drawer.h);
+            drawer.elementWidth = option.eleWidth * drawer.width;
 
         },
         animate() {
@@ -20,7 +25,14 @@
             requestAnimationFrame(drawer.animate);
         },
         draw() {
-
+            let ctx = drawer.ctx;
+            ctx.clearRect(0, 0, drawer.w, drawer.h);
+            ctx.fillStyle = option.backgroundColor;
+            ctx.fillRect(0, 0, drawer.w, drawer.h);
+            ctx.save();
+            ctx.translate(drawer.w / 2, drawer.h / 2);
+            ctx.restore();
+            CanvasUtil.drawMark(ctx, drawer.mark);
         },
         update() {
 
