1d17cd1bfe84550f9ba6575cc0a7432113149d34
ShenJinXiang <sjx-sword@163.com>
2021-06-04 08:54:08 +0800

    开始椭圆内反射

---
+++ public/202106/椭圆内反射/index.js
@@ -0,0 +1,10 @@
+{
+    const option = {
+        backgroundColor: '#000',
+        ellipseMajor: 0.4,
+        ellipseMinor: 0.25,
+        pointsLength: 50,
+        linePointsLength: 1000,
+        speed: 40,
+    }
+}
