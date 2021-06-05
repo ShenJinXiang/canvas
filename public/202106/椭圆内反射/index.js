be907cc7ac45f9ed6aaacf7ab967de5085cad3a3
shenjinxiang <sjx-sword@163.com>
2021-06-05 08:41:05 +0800

    df

--- public/202106/椭圆内反射/index.js
+++ public/202106/椭圆内反射/index.js
@@ -7,4 +7,14 @@
         linePointsLength: 1000,
         speed: 40,
     }
+
+    const drawer = {
+        start() {
+            drawer.c = document.getElementById('canvas');
+            drawer.ctx = drawer.c.getContext('2d');
+            drawer.mark = CanvasUtil.getMarkCanvas('#999');
+            drawer.init();
+            drawer.animate();
+        },
+    };
 }
