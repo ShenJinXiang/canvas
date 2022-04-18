c34b20465323db5d8286d98acffc3efde5e7b170
shenjinxiang <sjx-sword@163.com>
2022-04-19 07:09:28 +0800

    调整旧版代码

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -3,11 +3,13 @@
     <div class="main-container">
       <div class="title-con">
         <h1>CANVAS</h1>
-        <a target="_self" href="/index-old.html">旧版</a>
+        <a target="_self" href="/bck/index.html">旧版</a>
       </div>
       <ul>
         <li v-for="(item, index) in pages" :key="index">
-          <router-link :to="`/page/${item.path}`">{{ index + 1 }}. {{ item.title }}</router-link>
+          <router-link :to="`/page/${item.path}`"
+            >{{ index + 1 }}. {{ item.title }}</router-link
+          >
         </li>
       </ul>
     </div>
@@ -15,6 +17,6 @@
   </div>
 </template>
 <script lang="ts" setup>
-import vFooter from '@/components/layout/Footer.vue';
-import pages from '@/router/page';
+import vFooter from "@/components/layout/Footer.vue";
+import pages from "@/router/page";
 </script>
