aaf250618e1502d1901805cf9b3347b98374d273
shenjinxiang <sjx-sword@163.com>
2022-04-20 16:28:23 +0800

    修改国旗布局

--- src/views/page/FlagChina.vue
+++ src/views/page/FlagChina.vue
@@ -1,42 +1,27 @@
 <template>
   <div class="flagChina-con">
-    <div class="flag-box">
+    <div class="flag-box" @click="showContro = true">
       <flag-china :show-lines="showLines" :width="width"></flag-china>
     </div>
-    <div class="contro-con">
-      <el-row>
-        <el-col :span="2" :offset="6">
-          <el-button type="primary" @click="showLines = !showLines">{{ showLines ? '隐藏辅助线' : '显示辅助线'}}</el-button>
-        </el-col>
-        <el-col :span="10">
-          <el-slider v-model="width" :min="minWidth" :max="maxWidth" :step="10" />
-        </el-col>
-      </el-row>
-    </div>
+    <el-drawer v-model="showContro" title="设置" :direction="direction">
+      <el-checkbox v-model="showLines" label="显示辅助线" />
+      <el-slider
+        v-model="width"
+        :min="minWidth"
+        :max="maxWidth"
+        :step="10"
+      />
+    </el-drawer>
   </div>
 </template>
 <script lang="ts" setup>
-import FlagChina from '@/components/page/2020/FlagChina/index.vue';
-import { ref, Ref } from 'vue';
+import FlagChina from "@/components/page/2020/FlagChina/index.vue";
+import { ref, Ref } from "vue";
 
+const showContro = ref(false);
+const direction = ref("rtl");
 const showLines: Ref<boolean> = ref(false);
 const width: Ref<number> = ref(900);
-const maxWidth: number = 1200;
+const maxWidth: number = Math.min(window.innerWidth - 40, (window.innerHeight - 20) * 1.5 - 40);
 const minWidth: number = 400;
 </script>
-<style lang="less">
-@width: 1200px;
-.flagChina-con {
-  .flag-box {
-    width: @width;
-    height: @width * 2 / 3;
-    margin: 20px auto;
-    canvas {
-      display: block;
-    }
-  }
-  .contro-con {
-    padding-bottom: 20px;
-  }
-}
-</style>
