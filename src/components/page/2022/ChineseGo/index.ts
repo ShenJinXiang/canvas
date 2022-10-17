a01087a8def91498a1da34ffd559683c2b156696
ShenJinXiang <sjx-sword@163.com>
2022-10-17 08:10:36 +0800

    d

--- src/components/page/2022/ChineseGo/index.ts
+++ src/components/page/2022/ChineseGo/index.ts
@@ -110,7 +110,7 @@ export default class ChineseGo extends Animate {
   };
   constructor(gridWidth: number) {
     super();
-    this.size = 15;
+    this.size = 13;
     this.gridWidth = gridWidth;
     this.pieceRadius = this.gridWidth * 0.3;
     this.goStarRadius = this.gridWidth * 0.1;
