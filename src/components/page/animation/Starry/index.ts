d6da1f0ec5692b240ae4ae29601276f973858e25
shenjinxiang <sjx-sword@163.com>
2022-12-12 07:04:01 +0800

    星空背景动画

--- src/components/page/animation/Starry/index.ts
+++ src/components/page/animation/Starry/index.ts
@@ -1,14 +1,84 @@
 import Animate from "@/lib/Animate";
+import { random } from "@/lib/Kit";
 
+interface IOption {
+  density: number;
+  densitySize: number;
+  velocityX: number;
+  velocityY: number;
+  particleMaxRadius: number;
+  particleColor: string;
+}
+class Particle {
+  ox: number;
+  oy: number;
+  radius: number;
+  constructor(ox: number, oy: number, radius: number) {
+    this.ox = ox;
+    this.oy = oy;
+    this.radius = radius;
+  }
+
+  update(vx: number, vy: number) {
+    this.ox += vx;
+    this.oy += vy;
+  }
+  isCrossBorder(): boolean {
+    return this.oy <= 0;
+  }
+  draw(context: CanvasRenderingContext2D | null, fillStyle: string) {
+    if (!context) {
+      return;
+    }
+    context.save();
+    context.translate(this.ox, this.oy);
+    context.scale(this.radius, this.radius);
+    context.fillStyle = fillStyle;
+    context.beginPath();
+    context.arc(0, 0, 1, 0, 2 * Math.PI, false);
+    context.closePath();
+    context.fill();
+    context.restore();
+  }
+}
 export default class Starry extends Animate {
   canvas: HTMLCanvasElement | null = null;
   context: CanvasRenderingContext2D | null = null;
   width: number;
   height: number;
+  // 粒子数量
+  particleBuffLength: number = 0;
+  particles: Particle[] = [];
+  count: number;
+  option: IOption = {
+    density: 3,
+    densitySize: 100,
+    velocityX: -1,
+    velocityY: -0.5,
+    particleMaxRadius: 2,
+    particleColor: '#fff'
+  }
   constructor(width: number, height: number) {
     super();
     this.width = width;
     this.height = height;
+    this.count = 0;
+    this.initData();
+  }
+
+  private initData(): this {
+    const particleNumber = Math.round((this.option.density * this.width * this.height) / (this.option.densitySize * this.option.densitySize));
+    this.particleBuffLength = Math.round(this.option.density * this.width / this.option.densitySize);
+    this.particles = [];
+    for (let i = 0; i < particleNumber; i++) {
+      this.particles.push(new Particle(
+        random(this.width * 2),
+        // random(this.height),
+        random(this.height + this.option.densitySize),
+        random(this.option.particleMaxRadius)
+      ));
+    }
+    return this;
   }
   public initCanvas(canvas: HTMLCanvasElement): this {
     if (!canvas) {
@@ -37,7 +107,27 @@ export default class Starry extends Animate {
   }
 
   update() {
-
+    this.count++;
+    if (this.count >= Math.abs(this.option.densitySize / this.option.velocityY)) {
+      for (let i = 0; i < this.particleBuffLength; i++) {
+        this.particles.push(new Particle(
+          random(this.width * 2),
+          random(this.height, this.height + this.option.densitySize),
+          random(this.option.particleMaxRadius)
+        ));
+      }
+      this.count = 0;
+    }
+    this.particles.forEach((item) => item.update(this.option.velocityX, this.option.velocityY));
+    let cnt = 0
+    for (let i = 0; i < this.particles.length; i++) {
+      if (!this.particles[i].isCrossBorder()) {
+        this.particles[cnt++] = this.particles[i];
+      }
+    }
+    while (this.particles.length > cnt) {
+      this.particles.pop();
+    }
   }
 
   draw() {
@@ -45,10 +135,7 @@ export default class Starry extends Animate {
       return this;
     }
     this.clear();
-    this.context.save();
-    this.context.fillStyle = 'red';
-    this.context.fillRect(100, 100, 200, 120);
-    this.context.restore();
+    this.particles.forEach((item) => item.draw(this.context, this.option.particleColor));
   }
 
 }
