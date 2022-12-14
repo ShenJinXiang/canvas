66b83a7aa32b758b720c381241cbbcc9105bd920
ShenJinXiang <sjx-sword@163.com>
2022-12-14 11:57:04 +0800

    dfd

--- src/components/page/animation/SevenPiecePuzzleAnimation/index.ts
+++ src/components/page/animation/SevenPiecePuzzleAnimation/index.ts
@@ -1,15 +1,16 @@
 import Animate from "@/lib/Animate";
 
-const { PI } = Math.PI;
-const _90_DEG = PI / 2;
-const _135_DEG = 3 * PI / 4;
-const _180_DEG = PI;
-const _225_DEG = 5 * PI / 4;
-const _270_DEG = 3 * PI / 2;
+const PI: number = Math.PI;
+const ONE_DEG: number = PI / 180;
+const PI_4: number = PI / 4;
+const PI_2: number = PI / 2;
+const PI_3_4: number = 3 * PI / 4;
+const PI_5_4: number = 5 * PI / 4;
+const PI_3_2 = 3 * PI / 2;
 const SQRT_2 = Math.sqrt(2);
 const SQRT_2_HALF = Math.sqrt(2) / 2;
 interface PiecePosition {
-  show?: boolean;
+  hide?: boolean;
   sx: number;
   sy: number;
   rotate: number;
@@ -21,8 +22,8 @@ class Piece {
     this.size = size;
     this.fillStyle = fillStyle;
   }
-  draw(context: CanvasRenderingContext2D | null, { sx, sy, rotate, show = true }: PiecePosition) {
-    if (!context || !show) {
+  draw(context: CanvasRenderingContext2D | null, { sx, sy, rotate, hide }: PiecePosition) {
+    if (!context || hide) {
       return;
     }
     context.save();
@@ -35,6 +36,13 @@ class Piece {
     context.shadowBlur = this.size * 0.15;
     this.createPath(context);
     context.fill();
+
+    context.strokeStyle = '#fff';
+    context.lineWidth = 2;
+    // context.lineJoin = 'bevel';
+    context.lineJoin = 'round';
+    this.createPath(context);
+    context.stroke();
     context.restore();
   }
   createPath(context: CanvasRenderingContext2D) { }
@@ -70,10 +78,10 @@ class Parallelogram1 extends Piece {
   }
   createPath(context: CanvasRenderingContext2D) {
     context.beginPath();
-    context.moveTo(-this.size / Math.sqrt(2), -this.size / Math.sqrt(2));
+    context.moveTo(-this.size / SQRT_2, -this.size / SQRT_2);
     context.lineTo(0, 0);
-    context.lineTo(this.size * Math.sqrt(2), 0);
-    context.lineTo(this.size / Math.sqrt(2), -this.size / Math.sqrt(2));
+    context.lineTo(this.size * SQRT_2, 0);
+    context.lineTo(this.size / SQRT_2, -this.size / SQRT_2);
     context.closePath();
   }
 }
@@ -105,25 +113,34 @@ export default class SevenPiecePuzzleAnimation extends Animate {
   height: number;
   size: number;
   pieces: Piece[] = [];
+  combinations: PiecePosition[][] = [];
   // 粒子数量
   constructor(width: number, height: number) {
     super();
     this.width = width;
     this.height = height;
     this.size = 100;
+    this.initCombinations();
     this.initData();
   }
 
+  private initCombinations(): this {
+    this.combinations = [
+      [{ sx: 0, sy: SQRT_2, rotate: PI_3_4 }, { sx: 0, sy: SQRT_2, rotate: -PI_3_4 }, { sx: SQRT_2_HALF, sy: SQRT_2_HALF, rotate: -PI_4 }, { sx: 0, sy: SQRT_2, rotate: PI_4 }, { sx: SQRT_2, sy: 2 * SQRT_2, rotate: PI }, { sx: 0, sy: SQRT_2, rotate: -PI_4 }, { sx: SQRT_2_HALF, sy: 3 * SQRT_2_HALF, rotate: 0, hide: true }, { sx: -SQRT_2_HALF, sy: 3 * SQRT_2_HALF, rotate: 0 }],
+      [{ sx: 0, sy: SQRT_2, rotate: -PI_3_4 }, { sx: -SQRT_2, sy: 0, rotate: PI_4 }, { sx: 3 * SQRT_2_HALF, sy: SQRT_2_HALF, rotate: PI_4 }, { sx: SQRT_2_HALF, sy: SQRT_2_HALF, rotate: PI_4 }, { sx: SQRT_2, sy: SQRT_2 + 1, rotate: -PI_3_4 }, { sx: SQRT_2_HALF, sy: SQRT_2_HALF, rotate: -PI_4 }, { sx: -SQRT_2, sy: 2 * SQRT_2, rotate: PI_2, hide: true }, { sx: -SQRT_2_HALF, sy: 3 * SQRT_2_HALF, rotate: PI_2 }]
+    ];
+    return this;
+  }
   private initData(): this {
     this.pieces = [
-      new Triangle(this.size * 2, 'hsla(50, 75%, 60%, 1)'),
-      new Triangle(this.size * 2, 'hsla(270, 75%, 60%, 1)'),
-      new Triangle(this.size, 'hsla(230, 75%, 60%, 1)'),
-      new Triangle(this.size, 'hsla(120, 75%, 60%, 1)'),
-      new Triangle(this.size * Math.sqrt(2), 'hsla(40, 75%, 60%, 1)'),
-      new Square(this.size, 'hsla(190, 75%, 60%, 1)'),
-      new Parallelogram1(this.size, 'hsla(0, 75%, 60%, 1)'),
-      new Parallelogram2(this.size, 'hsla(0, 75%, 60%, 1)'),
+      new Triangle(this.size * 2, 'hsla(50, 75%, 60%, 1)'), // 黄色大三角
+      new Triangle(this.size * 2, 'hsla(270, 75%, 60%, 1)'), // 紫色大三角
+      new Triangle(this.size, 'hsla(230, 75%, 60%, 1)'), // 蓝色小三角
+      new Triangle(this.size, 'hsla(120, 75%, 60%, 1)'), // 绿色小三角
+      new Triangle(this.size * SQRT_2, 'hsla(30, 75%, 60%, 1)'), // 橙色中三角
+      new Square(this.size, 'hsla(190, 75%, 60%, 1)'), // 正方形
+      new Parallelogram1(this.size, 'hsla(0, 75%, 60%, 1)'), // 平行四边形1
+      new Parallelogram2(this.size, 'hsla(0, 75%, 60%, 1)'), // 平行四边形2
     ];
     return this;
   }
@@ -161,28 +178,11 @@ export default class SevenPiecePuzzleAnimation extends Animate {
     this.context.save();
     this.context.translate(this.width / 2, this.height * 0.8);
     this.context.scale(1, -1);
-    this.pieces[0].draw(this.context, { sx: 0, sy: this.size * Math.sqrt(2), rotate: Math.PI * 0.75 });
-    this.pieces[1].draw(this.context, { sx: 0, sy: this.size * Math.sqrt(2), rotate: -Math.PI * 0.75 });
-    this.pieces[2].draw(this.context, { sx: this.size / Math.sqrt(2), sy: this.size / Math.sqrt(2), rotate: -Math.PI / 4 });
-    this.pieces[3].draw(this.context, { sx: 0, sy: this.size * Math.sqrt(2), rotate: Math.PI / 4 });
-    this.pieces[4].draw(this.context, { sx: this.size * Math.sqrt(2), sy: 2 * this.size * Math.sqrt(2), rotate: Math.PI });
-    this.pieces[5].draw(this.context, { sx: 0, sy: this.size * Math.sqrt(2), rotate: -Math.PI / 4 });
-    this.pieces[7].draw(this.context, { sx: -this.size / Math.sqrt(2), sy: 3 * this.size / Math.sqrt(2), rotate: 0 });
+
+    this.combinations[1].forEach((pos, index) => {
+      this.pieces[index].draw(this.context, { sx: pos.sx * this.size, sy: pos.sy * this.size, rotate: pos.rotate, hide: pos.hide });
+    })
     this.context.restore();
-
-    // this.pieces[6].draw(this.context, { sx: 200, sy: 200, rotate: 0 });
-    // this.pieces[7].draw(this.context, { sx: 400, sy: 200, rotate: 0 });
-    // this.pieces[0].draw(this.context, { sx: 50, sy: 50, rotate: 0 });
-    // this.pieces[1].draw(this.context, { sx: 300, sy: 50, rotate: 0 });
-    // this.pieces[2].draw(this.context, { sx: 550, sy: 50, rotate: 0 });
-    // this.pieces[3].draw(this.context, { sx: 700, sy: 50, rotate: 0 });
-    // this.pieces[4].draw(this.context, { sx: 50, sy: 300, rotate: 0 });
-    // this.pieces[5].draw(this.context, { sx: 300, sy: 300, rotate: 0 });
-    // this.pieces[6].draw(this.context, { sx: 550, sy: 300, rotate: 0 });
-    // this.pieces[7].draw(this.context, { sx: 700, sy: 300, rotate: 0 });
-
-    // this.pieces[6].draw(this.context, { sx: 550, sy: 300, rotate: Math.PI / 2 });
-    // this.pieces[7].draw(this.context, { sx: 700, sy: 300, rotate: Math.PI / 2 });
   }
 
 }
