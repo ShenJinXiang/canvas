607c3433b2e23d9d8c0f00685cfc855d185e2295
shenjinxiang <sjx-sword@163.com>
2022-12-27 08:14:39 +0800

    df

--- src/views/page/animation/LaserEffect.vue
+++ src/views/page/animation/LaserEffect.vue
@@ -1,11 +1,11 @@
 <template>
   <div>
     <Laser-effect
-      :width="controForm.width"
-      :height="controForm.height"
-      :laster-number="controForm.lasterNumber"
-      @click="showContro = true"></Laser-effect>
-    <el-drawer v-model="showContro" title="设置" :direction="direction">
+      :width="laserWidth"
+      :height="laserHeight - 40"
+      :laster-number="240"
+      ></Laser-effect>
+    <!-- <el-drawer v-model="showContro" title="设置" :direction="direction">
       <el-form :module="controForm" label-width="120px">
         <el-form-item label="宽" prop="width">
           <el-slider v-model="controForm.width" :min="maxWidth / 2" :max="maxWidth" />
@@ -17,20 +17,23 @@
           <el-slider v-model="controForm.lasterNumber" :min="40" :max="400" />
         </el-form-item>
       </el-form>
-    </el-drawer>
+    </el-drawer> -->
   </div>
 </template>
 <script lang="ts" setup>
 import LaserEffect from '@/components/page/animation/LaserEffect/index.vue';
-import { reactive, ref } from 'vue';
+import { useWindowSize } from '@vueuse/core';
+import { ref } from 'vue';
 
-const showContro = ref(false);
-const direction = ref("rtl");
-const maxWidth = window.innerWidth;
-const maxHeight = window.innerHeight - 40;
-const controForm = reactive({
-  width: maxWidth,
-  height: maxHeight,
-  lasterNumber: 200
-});
+const { width: laserWidth, height: laserHeight } = useWindowSize();
+const lasterNumber = ref(240);
+// const showContro = ref(false);
+// const direction = ref("rtl");
+// const maxWidth = window.innerWidth;
+// const maxHeight = window.innerHeight - 40;
+// const controForm = reactive({
+//   width: maxWidth,
+//   height: maxHeight,
+//   lasterNumber: 200
+// });
 </script>
