27d7ae78c063a0f970fc32d8b821661a3ff3d56e
shenjinxiang <sjx-sword@163.com>
2023-01-06 22:12:25 +0800

    hur

--- src/components/page/game/HuaRongRoad/index.ts
+++ src/components/page/game/HuaRongRoad/index.ts
@@ -1,5 +1,50 @@
+interface IOption {
+  backgroundColor: string;
+}
 export class HuaRongRoad {
-  constructor() {
+  canvas: HTMLCanvasElement | null = null;
+  context: CanvasRenderingContext2D | null = null;
+  width: number;
+  height: number;
+  private option: IOption = {
+    backgroundColor: '#fff'
+  };
+  side: number;
+  constructor(side: number) {
+    this.side = side;
+    this.width = this.side * 5;
+    this.height = this.side * 5;
+  }
+  initCanvas(canvas: HTMLCanvasElement): this {
+    if (!canvas) {
+      throw new Error('初始化canvas错误：对象为空！');
+    }
+    this.canvas = canvas;
+    this.canvas.width = this.width;
+    this.canvas.height = this.height;
+    this.context = this.canvas.getContext('2d');
+    return this;
+  }
+  draw() {
+    if (!this.context) {
+      return;
+    }
+    this.clear();
+    this.context.save();
+    this.context.fillStyle = 'red';
+    this.context.fillRect(100, 100, 200, 130);
+    this.context.restore();
 
+  }
+  private clear(): this {
+    if (!this.canvas || !this.context) {
+      return this;
+    }
+    this.context.save();
+    this.context.clearRect(0, 0, this.canvas.width, this.canvas.height);
+    this.context.fillStyle = this.option.backgroundColor;
+    this.context.fillRect(0, 0, this.canvas.width, this.canvas.height);
+    this.context.restore();
+    return this;
   }
 }
--- src/components/page/game/HuaRongRoad/index.vue
+++ src/components/page/game/HuaRongRoad/index.vue
@@ -1,5 +1,16 @@
 <template>
-  <div>HuaRongRoad</div>
+  <div>
+    <canvas ref="canvasRef"></canvas>
+  </div>
 </template>
 <script lang="ts" setup>
+import { Ref, onMounted, ref } from 'vue';
+import { HuaRongRoad } from '.';
+
+const width = 400;
+const canvasRef: Ref = ref();
+const canvas = new HuaRongRoad(width);
+onMounted(() => {
+  canvas.initCanvas(canvasRef.value).draw();
+});
 </script>
