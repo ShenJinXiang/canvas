ba7e2c49345280a557a4016b5eb4e626f66e9aac
ShenJinXiang <sjx-sword@163.com>
2023-01-28 10:49:34 +0800

    开始绘制彩色烟花动画 color-fireworks

---
+++ src/components/page/animation/ColorFireworks/index.vue
@@ -0,0 +1,27 @@
+<template>
+  <div class="container">
+    <canvas ref="canvasRef"></canvas>
+  </div>
+</template>
+<script lang="ts" setup>
+import { onMounted, ref } from 'vue';
+import ColorFireworks from '.';
+const props = defineProps({
+  width: {
+    type: Number,
+    required: true
+  },
+  height: {
+    type: Number,
+    required: true
+  }
+});
+
+const canvasRef = ref();
+const colorFireworks = new ColorFireworks(props.width, props.height);
+
+onMounted(() => {
+  colorFireworks.initCanvas(canvasRef.value).run();
+});
+
+</script>
---
+++ src/views/page/animation/ColorFireworks.vue
@@ -0,0 +1,9 @@
+<template>
+  <color-fireworks :width="width" :height="height - 40"></color-fireworks>
+</template>
+<script lang="ts" setup>
+import ColorFireworks from '@/components/page/animation/ColorFireworks/index.vue';
+import { useWindowSize } from '@vueuse/core';
+
+const { width, height } = useWindowSize();
+</script>
