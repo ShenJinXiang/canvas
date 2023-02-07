b5aa066e752848b2164fae0a87bc5b6e91636d65
ShenJinXiang <sjx-sword@163.com>
2023-02-07 09:55:07 +0800

    rotate line animation add point class

--- src/components/page/animation/RotateLineAnimation/index.ts
+++ src/components/page/animation/RotateLineAnimation/index.ts
@@ -1,10 +1,39 @@
 import Animate from "@/lib/Animate";
 
+class Point {
+  x: number;
+  y: number;
+  constructor(x: number, y: number) {
+    this.x = x;
+    this.y = y;
+  }
+  draw(context: CanvasRenderingContext2D | null, radius: number, color: string) {
+    if (!context) {
+      return;
+    }
+    context.save();
+    context.beginPath();
+    context.fillStyle = color;
+    context.arc(this.x, this.y, radius, 0, 2 * Math.PI, false);
+    context.fill();
+    context.restore();
+  }
+}
 export class RotateLineAnimation extends Animate {
+  sideNumber: number;
+  pointNumber: number;
+  points: Point[] = [];
   constructor(width: number, height: number) {
     super();
     this.width = width;
     this.height = height;
+    this.sideNumber = 5;
+    this.pointNumber = 15;
+    this.initData();
+  }
+
+  private initData() {
+    this.points = [];
   }
 
   draw() {
