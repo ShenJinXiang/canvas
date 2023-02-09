a20ce7c2832b11db71e90f55f0cb456b7ba5dc94
ShenJinXiang <sjx-sword@163.com>
2023-02-09 09:16:21 +0800

    RotateLineAnimation 添加动画控制

--- src/components/page/animation/RotateLineAnimation/index.vue
+++ src/components/page/animation/RotateLineAnimation/index.vue
@@ -4,7 +4,7 @@
   </div>
 </template>
 <script lang="ts" setup>
-import { onMounted, ref } from 'vue';
+import { onMounted, ref, watch } from 'vue';
 import { RotateLineAnimation } from '.';
 
 const props = defineProps({
@@ -15,13 +15,45 @@ const props = defineProps({
   height: {
     type: Number,
     required: true
+  },
+  sideNumber: {
+    type: Number,
+    required: true
+  },
+  pointNumber: {
+    type: Number,
+    required: true
   }
 });
 const canvasRef = ref();
-const rotateLineAnimation = new RotateLineAnimation(props.width, props.height);
+const rotateLineAnimation = new RotateLineAnimation(props.width, props.height, props.sideNumber, props.pointNumber);
 
 onMounted(() => {
   rotateLineAnimation.initCanvas(canvasRef.value).run();
 });
 
+watch(
+  () => props.width,
+  () => {
+    rotateLineAnimation.setRect(props.width, props.height);
+  }
+);
+watch(
+  () => props.height,
+  () => {
+    rotateLineAnimation.setRect(props.width, props.height);
+  }
+);
+watch(
+  () => props.sideNumber,
+  () => {
+    rotateLineAnimation.setSideNumber(props.sideNumber);
+  }
+);
+watch(
+  () => props.pointNumber,
+  () => {
+    rotateLineAnimation.setPointNumber(props.pointNumber);
+  }
+);
 </script>
