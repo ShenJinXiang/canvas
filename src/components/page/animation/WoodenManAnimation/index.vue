fd87a7e25483de18b80f58e010063d398f578130
ShenJinXiang <sjx-sword@163.com>
2023-02-13 09:00:02 +0800

    开始 WoodenManAnimation

---
+++ src/components/page/animation/WoodenManAnimation/index.vue
@@ -0,0 +1,24 @@
+<template>
+  <canvas ref="canvasRef"></canvas>
+</template>
+<script lang="ts" setup>
+import { onMounted, ref } from 'vue';
+import WoodenManAnimation from '.';
+
+const props = defineProps({
+  width: {
+    typs: Number,
+    required: true
+  },
+  height: {
+    typs: Number,
+    required: true
+  }
+});
+const canvasRef = ref();
+
+const woodenManAnimation = new WoodenManAnimation(props.width, props.height);
+onMounted(() => {
+  woodenManAnimation.initCanvas(canvasRef.value).run();
+});
+</script>
