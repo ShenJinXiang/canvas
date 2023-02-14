2cf53f8bfdede38c10f1700dfab3d5c0a29d1087
shenjinxiang <sjx-sword@163.com>
2023-02-14 21:34:28 +0800

    WoodenManAnimation: add FiexedPoint

--- src/components/page/animation/WoodenManAnimation/index.ts
+++ src/components/page/animation/WoodenManAnimation/index.ts
@@ -6,15 +6,40 @@ interface IOption {
   fixedPointColor: string;
 }
 
+class FixedPoint {
+  x: number;
+  y: number;
+  constructor(x: number, y: number) {
+    this.x = x;
+    this.y = y;
+  }
+  draw(context: CanvasRenderingContext2D | null, radius: number, style: string) {
+    if (!context) {
+      return;
+    }
+    context.save();
+    context.beginPath();
+    context.fillStyle = style;
+    context.arc(this.x, this.y, radius, 0, 2 * Math.PI, false);
+    context.fill();
+    context.restore();
+  }
+}
+
 class Element {
+  // rotate: number;
+  // fixedPoints: FixedPoint[];
+  constructor() {
+
+  }
 
 }
 
 export default class WoodenManAnimation extends Animate {
   private option: IOption = {
-    backgroundColor: '#fff',
+    backgroundColor: '#f1f1f1',
     bodyColor: '#333',
-    fixedPointColor: '#f1f1f1'
+    fixedPointColor: '#e1e1e1'
   };
   constructor(width: number, height: number) {
     super();
@@ -25,7 +50,7 @@ export default class WoodenManAnimation extends Animate {
     if (!this.context) {
       return;
     }
-    this.clear();
+    this.clear(this.option.backgroundColor);
     this.context.save();
     this.context.fillStyle = '#084';
     this.context.fillRect(200, 200, 360, 200);
