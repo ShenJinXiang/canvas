8ca9f5085a0a48608f660ce78ca6796d92200508
ShenJinXiang <sjx-sword@163.com>
2023-03-08 15:36:39 +0800

    SimplePendulumAnimation: add SimplePendulum class

--- src/components/page/animation/SimplePendulumAnimation/index.ts
+++ src/components/page/animation/SimplePendulumAnimation/index.ts
@@ -4,6 +4,50 @@ interface IOption {
   backgroundColor: string;
   lineColor: string;
 }
+class SimplePendulum {
+  ox: number;
+  oy: number;
+  lineLength: number;
+  bottomRadius: number;
+  bottomAngleStep: number;
+  currentButtomAngle: number;
+  ballX: number = 0;
+  ballY: number = 0;
+  constructor(ox: number, oy: number, lineLength: number, maxAngle: number) {
+    this.ox = ox;
+    this.oy = oy;
+    this.lineLength = lineLength;
+    this.bottomRadius = this.lineLength * Math.sin(maxAngle);
+    this.bottomAngleStep = 2 * Math.PI / this.bottomRadius;
+    this.currentButtomAngle = 0;
+  }
+  update() {
+    this.currentButtomAngle += this.bottomAngleStep;
+    this.ballX = this.bottomRadius * Math.cos(this.currentButtomAngle);
+    const a = Math.asin(this.ballX / this.lineLength);
+    this.ballY = this.lineLength * Math.sin(a);
+  }
+  draw(context: CanvasRenderingContext2D | null, lineColor: string, ballColor: string, ballRadius: number) {
+    if (!context) {
+      return;
+    }
+    context.save();
+    context.translate(this.ox, this.oy);
+
+    context.beginPath();
+    context.strokeStyle = lineColor;
+    context.moveTo(0, 0);
+    context.lineTo(this.ballX, this.ballY);
+    context.stroke();
+
+    context.beginPath();
+    context.fillStyle = ballColor;
+    context.arc(this.ballX, this.ballY, ballRadius, 0, 2 * Math.PI, false);
+    context.fill();
+
+    context.restore();
+  }
+}
 export default class SimplePendulumAnimation extends Animate {
   private option: IOption = {
     backgroundColor: '#000',
