51c4fd0a6ed846bdc96a034bb3425549572b3275
ShenJinXiang <sjx-sword@163.com>
2023-03-20 19:12:47 +0800

    CardioidAnimation1: add Element class

--- src/components/page/animation/CardioidAnimation1/index.ts
+++ src/components/page/animation/CardioidAnimation1/index.ts
@@ -7,8 +7,74 @@ interface IOption {
   elementNumberRatio: number;
 }
 
+class Element {
+  private x: number;
+  private y: number;
+  private color: string;
+  private angle: number;
+  private angleStep: number;
+  private roundRadius: number;
+  private size: number;
+  constructor(x: number, y: number, roundRadius: number, size: number, color: string, roundTime: number) {
+    this.x = x;
+    this.y = y;
+    this.color = color;
+    this.angle = 0;
+    this.angleStep = 2 * Math.PI / roundTime;
+    this.roundRadius = roundRadius;
+    this.size = size;
+  }
+  public update() {
+    this.angle += this.angleStep;
+  }
+  public position(x: number, y: number, roundRadius: number): this {
+    this.x = x;
+    this.y = y;
+    return this;
+  }
+  public setRoundRadius(roundRadius: number): this {
+    this.roundRadius = roundRadius;
+    return this;
+  }
+  public setSize(size: number): this {
+    this.size = size;
+    return this;
+  }
+  public draw(context: CanvasRenderingContext2D | null) {
+    if (!context) {
+      return;
+    }
+    context.save();
+    context.translate(this.x + this.roundRadius * Math.cos(this.angle), this.y + this.roundRadius * Math.sin(this.angle));
+    context.strokeStyle = this.color;
+    context.fillStyle = this.color;
+    this.path(context);
+    context.restore();
+  }
+
+  private path(context: CanvasRenderingContext2D) { }
+}
+
+class CircleElement extends Element {
+  constructor(x: number, y: number, roundRadius: number, size: number, color: string, roundTime: number) {
+    super(x, y, roundRadius, size, color, roundTime);
+  }
+  private path(context: CanvasRenderingContext2D) {
+
+  }
+}
+class RectElement extends Element {
+  constructor(x: number, y: number, roundRadius: number, size: number, color: string, roundTime: number) {
+    super(x, y, roundRadius, size, color, roundTime);
+  }
+  private path(context: CanvasRenderingContext2D) {
+
+  }
+}
+
 export default class CardioidAnimation extends Animate {
 
+  private elements: Element[] = [];
   private option: IOption = {
     minRoundTime: 50,
     maxRoundTime: 100,
