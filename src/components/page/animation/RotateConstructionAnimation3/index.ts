8c3c1f96b1460f10a215e1117f7e998e6f8b0d27
ShenJinXiang <sjx-sword@163.com>
2023-04-12 09:57:20 +0800

    RotateConstructionAnimation3: 调整元素代码（初始化、相对位置、绘制） 添加简单动画例子

--- src/components/page/animation/RotateConstructionAnimation3/index.ts
+++ src/components/page/animation/RotateConstructionAnimation3/index.ts
@@ -4,6 +4,9 @@ import Point from "@/lib/Point";
 enum ElementPostionType {
   M, L, R
 }
+enum TaskStatus {
+  PREPARE, DOING, COMPLETE
+}
 
 class Element {
   roundRadius: number;
@@ -12,28 +15,60 @@ class Element {
   lineColor: string;
   pointColor: string;
   pointRadius: number;
-  leftPoint: Point;
-  rightPoint: Point;
-  middelPoint: Point;
+  start: Point = { x: 0, y: 0 };
+  end: Point = { x: 0, y: 0 };
+  leftPoint: Point = { x: 0, y: 0 };
+  rightPoint: Point = { x: 0, y: 0 };
+  middelPoint: Point = { x: 0, y: 0 };
   positionPoint: Point;
   positionType: ElementPostionType;
-  constructor(roundRadius: number, lineWidth: number, lineColor: string, pointColor: string) {
+  constructor(roundRadius: number, lineWidth: number, lineColor: string, pointColor: string, positionType: ElementPostionType = ElementPostionType.M, position: Point = { x: 0, y: 0 }) {
     this.roundRadius = roundRadius;
     this.lineWidth = lineWidth;
     this.lineColor = lineColor;
     this.pointColor = pointColor;
     this.pointRadius = this.lineWidth * 0.35;
-    this.leftPoint = { x: -this.roundRadius, y: 0 };
-    this.middelPoint = { x: 0, y: 0 };
-    this.rightPoint = { x: this.roundRadius, y: 0 };
-    this.positionPoint = { x: 0, y: 0 };
-    this.positionType = ElementPostionType.M;
+    this.positionPoint = { x: position.x, y: position.y };
+    this.positionType = positionType;
+    this.refreshByPositionType();
+  }
+
+  private refreshByPositionType() {
+    switch (this.positionType) {
+      case ElementPostionType.L:
+        this.start = { x: -this.lineWidth, y: 0 };
+        this.leftPoint = { x: 0, y: 0 };
+        this.middelPoint = { x: this.roundRadius, y: 0 };
+        this.rightPoint = { x: 2 * this.roundRadius, y: 0 };
+        this.end = { x: 2 * this.roundRadius + this.lineWidth, y: 0 };
+        break;
+      case ElementPostionType.M:
+        this.start = { x: -this.roundRadius - this.lineWidth, y: 0 };
+        this.leftPoint = { x: -this.roundRadius, y: 0 };
+        this.middelPoint = { x: 0, y: 0 };
+        this.rightPoint = { x: this.roundRadius, y: 0 };
+        this.end = { x: this.roundRadius + this.lineWidth, y: 0 };
+        break;
+      case ElementPostionType.R:
+        this.start = { x: -2 * this.roundRadius - this.lineWidth, y: 0 };
+        this.leftPoint = { x: -2 * this.roundRadius, y: 0 };
+        this.middelPoint = { x: -this.roundRadius, y: 0 };
+        this.rightPoint = { x: 0, y: 0 };
+        this.end = { x: this.lineWidth, y: 0 };
+        break;
+    }
+
   }
 
   position(positionType: ElementPostionType, x: number, y: number) {
     this.positionType = positionType;
     this.positionPoint.x = x;
     this.positionPoint.y = y;
+    this.refreshByPositionType();
+  }
+
+  setRotate(rotate: number) {
+    this.rotate = rotate;
   }
 
   draw(context: CanvasRenderingContext2D | null) {
@@ -42,22 +77,13 @@ class Element {
     }
     context.save();
     context.translate(this.positionPoint.x, this.positionPoint.y);
-    switch (this.positionType) {
-      case ElementPostionType.L:
-        context.translate(-this.leftPoint.x, -this.leftPoint.y);
-        break;
-      case ElementPostionType.R:
-        context.translate(-this.rightPoint.x, -this.rightPoint.y);
-        break;
-      default:
-        context.translate(0, 0);
-    }
+    context.rotate(this.rotate);
     context.lineCap = 'round';
     context.beginPath();
     context.strokeStyle = this.lineColor;
     context.lineWidth = this.lineWidth;
-    context.moveTo(-this.roundRadius - this.lineWidth, 0);
-    context.lineTo(this.roundRadius + this.lineWidth, 0);
+    context.moveTo(this.start.x, this.start.y);
+    context.lineTo(this.end.x, this.end.y);
     context.stroke();
     this.drawPoint(context, this.middelPoint);
     this.drawPoint(context, this.leftPoint);
@@ -73,11 +99,65 @@ class Element {
   }
 }
 
-class LineAnimation {
-  constructor(element: Element, duration: number) { }
+class ElementTask {
+  private element: Element;
+  private position: Point;
+  private positionType: ElementPostionType;
+  private startAngle: number;
+  private endAngle: number;
+  private angleStep: number;
+  private duration: number;
+  private taskStatus: TaskStatus = TaskStatus.PREPARE;
+  private current: number = 0;
+  private counterclockwise: boolean;
+  constructor(element: Element, position: Point, positionType: ElementPostionType, startAngle: number, endAngle: number, duration: number, counterclockwise: boolean = false) {
+    this.element = element;
+    this.position = position;
+    this.positionType = positionType;
+    this.startAngle = startAngle;
+    this.endAngle = endAngle;
+    this.duration = duration;
+    this.angleStep = (this.endAngle - this.startAngle) / this.duration;
+    this.counterclockwise = counterclockwise;
+    this.reset();
+  }
+  update() {
+    if (this.taskStatus === TaskStatus.DOING) {
+      this.current++;
+      this.element.setRotate(this.startAngle + this.angleStep * this.current);
+      if (this.current >= this.duration) {
+        this.taskStatus = TaskStatus.COMPLETE;
+        this.current = this.duration;
+      }
+    }
+  }
+  start() {
+    this.taskStatus = TaskStatus.DOING;
+    this.current = 0;
+  }
+  reset() {
+    this.taskStatus = TaskStatus.PREPARE;
+    this.element.position(this.positionType, this.position.x, this.position.y);
+    this.current = 0;
+  }
+  draw(context: CanvasRenderingContext2D | null) {
+    if (!context) {
+      return;
+    }
+    switch (this.taskStatus) {
+      case TaskStatus.PREPARE:
+        break;
+      case TaskStatus.DOING:
+        break;
+      case TaskStatus.COMPLETE:
+        break;
+    }
+    this.element.draw(context);
+  }
 }
 
 export default class RotateConstructionAnimation extends Animate {
+  tasks: ElementTask[] = [];
   element: Element | null = null;
   constructor(width: number, height: number) {
     super();
@@ -86,7 +166,19 @@ export default class RotateConstructionAnimation extends Animate {
   }
 
   initData() {
-    this.element = new Element(200, 12, '#ccc', '#666');
+    const element = new Element(100, 12, '#ccc', '#666');
+    this.element = new Element(100, 12, '#ccc', '#666');
+    this.element.position(ElementPostionType.L, 600, 400);
+    this.element.setRotate(Math.PI / 6);
+    this.tasks = [
+      new ElementTask(element, { x: 400, y: 200 }, ElementPostionType.L, 0, Math.PI * 0.5, 100)
+    ]
+    this.tasks[0].start();
+
+  }
+
+  update() {
+    this.tasks.forEach((item) => item.update());
   }
 
   draw() {
@@ -95,8 +187,22 @@ export default class RotateConstructionAnimation extends Animate {
     }
     this.clear();
     this.context.save();
-    this.element?.position(ElementPostionType.M, 400, 200);
+    // this.element?.position(ElementPostionType.M, 400, 200);
+    // this.element?.draw(this.context);
+    this.tasks.forEach((item) => item.draw(this.context));
     this.element?.draw(this.context);
+    // this.element2?.draw(this.context);
+    // this.element3?.draw(this.context);
+
+    this.context.beginPath();
+    this.context.moveTo(500, 400);
+    this.context.lineTo(700, 400);
+    this.context.stroke();
+    this.context.beginPath();
+    this.context.moveTo(600, 300);
+    this.context.lineTo(600, 500);
+    this.context.stroke();
+
     this.context.restore();
   }
 
