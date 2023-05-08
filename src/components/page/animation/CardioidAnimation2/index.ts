4e4824dfae0d6f694b96161644cc9fadb6deb87d
ShenJinXiang <sjx-sword@163.com>
2023-05-08 19:43:10 +0800

    CardioidAnimation2: 完成动画

--- src/components/page/animation/CardioidAnimation2/index.ts
+++ src/components/page/animation/CardioidAnimation2/index.ts
@@ -1,11 +1,27 @@
 import Animate from "@/lib/Animate";
 
+interface IOption {
+  lineWidth: number;
+  color: string;
+  backgroundColor: string;
+};
+
 export default class CardioidAnimation extends Animate {
 
   private baseRadius: number = 0;
   private heartBuff: Uint32Array = new Uint32Array();
   private tempCanvas: HTMLCanvasElement | null = null;
   private tempContext: CanvasRenderingContext2D | null = null;
+
+  private option: IOption = {
+    lineWidth: 1,
+    color: '#fff',
+    backgroundColor: '#000'
+  };
+
+  private downStep: number = 0;
+  private step: number = 0;
+  private offsetY: number = 0;
 
   constructor(width: number, height: number) {
     super();
@@ -15,18 +31,51 @@ export default class CardioidAnimation extends Animate {
 
   private initData() {
     const base = Math.min(this.width, this.height);
+    this.step = base * 0.05 > 25 ? 25 : base * 0.05;
+    this.downStep = this.step * 0.4;
     this.baseRadius = base * 0.4 / 16;
     this.initTempCanvas();
     this.refreshHeartBuffer();
     console.log(this.heartBuff);
   }
 
+  update() {
+    this.offsetY += 0.5;
+  }
+
   draw() {
     if (!this.context) {
       return;
     }
-    this.clear();
+    this.clear(this.option.backgroundColor);
     this.context.save();
+    this.drawLines();
+    this.context.restore();
+  }
+
+  private drawLines() {
+    for (let y: number = 0; y < this.height; y += this.step) {
+      this.drawLine(y + this.offsetY % this.step);
+    }
+  }
+
+  private drawLine(y: number) {
+    if (!this.context) {
+      return;
+    }
+    this.context.save();
+    this.context.strokeStyle = this.option.color;
+    this.context.lineWidth = this.option.lineWidth;
+    this.context.translate(0, y);
+    this.context.beginPath();
+    for (let x = 0; x < this.width; x += 2) {
+      if (this.heartBuff[this.width * Math.floor(y) + x]) {
+        this.context.lineTo(x, this.downStep);
+      } else {
+        this.context.lineTo(x, 0);
+      }
+    }
+    this.context.stroke();
     this.context.restore();
   }
 
@@ -38,11 +87,11 @@ export default class CardioidAnimation extends Animate {
     ctx.translate(ox, oy);
     ctx.fillStyle = color;
     ctx.beginPath();
-    for (let angle = 0; angle < 2 * Math.PI; angle += 0.01 ) {
-        ctx.lineTo(
-            radius * 16 * Math.pow(Math.sin(angle), 3),
-            -radius * (13 * Math.cos(angle) - 5 * Math.cos(2 * angle) - 2 * Math.cos(3 * angle) - Math.cos(4 * angle))
-        );
+    for (let angle = 0; angle < 2 * Math.PI; angle += 0.01) {
+      ctx.lineTo(
+        radius * 16 * Math.pow(Math.sin(angle), 3),
+        -radius * (13 * Math.cos(angle) - 5 * Math.cos(2 * angle) - 2 * Math.cos(3 * angle) - Math.cos(4 * angle))
+      );
     }
     ctx.fill();
     ctx.restore();
