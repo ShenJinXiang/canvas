e902f6beac7660aa228a0b3ecb66673fa66b6047
ShenJinXiang <sjx-sword@163.com>
2023-05-16 22:19:10 +0800

    RotateConstructionAnimation4: 添加Element类

--- src/components/page/animation/RotateConstructionAnimation4/index.ts
+++ src/components/page/animation/RotateConstructionAnimation4/index.ts
@@ -2,11 +2,55 @@ import Animate from "@/lib/Animate";
 
 interface IOption {
   backgroundColor: string
+  elementNumber: number
 }
 
+class Element {
+  private x: number;
+  private y: number;
+  private radius: number;
+  private width: number;
+  private beginAngle: number;
+  private angleStep: number;
+  private style: string;
+  private angle: number;
+  constructor(x: number, y: number, radius: number, width: number, beginAngle: number, angleStep: number, style: string) {
+    this.x = x;
+    this.y = y;
+    this.radius = radius;
+    this.width = width;
+    this.beginAngle = beginAngle;
+    this.angleStep = angleStep;
+    this.style = style;
+    this.angle = this.beginAngle;
+  }
+  update() {
+    this.angle += this.angleStep;
+  }
+  draw(ctx: CanvasRenderingContext2D | null) {
+    if (!ctx) {
+      return;
+    }
+    ctx.save();
+    ctx.translate(this.x, this.y);
+    ctx.beginPath();
+    ctx.lineWidth = this.width;
+    ctx.strokeStyle = this.style;
+    ctx.arc(0, 0, this.radius, this.angle, Math.PI + this.angle, false);
+    ctx.stroke();
+    ctx.restore();
+  }
+  refresh(x: number, y: number, radius: number, width: number) {
+    this.x = x;
+    this.y = y;
+    this.radius = radius;
+    this.width = width;
+  }
+}
 export default class RotateConstructionAnimation extends Animate {
   private option: IOption = {
     backgroundColor: '#061928',
+    elementNumber: 30
   };
   constructor(width: number, height: number) {
     super();
@@ -15,6 +59,9 @@ export default class RotateConstructionAnimation extends Animate {
   }
 
   initData(): this {
+    const base = Math.min(this.width, this.height);
+    const radius = 0.4 * base;
+    const elementWidth = radius / this.option.elementNumber;
     return this;
   }
 
