141653e3116c6058db5c655ba306541c85ddee89
ShenJinXiang <sjx-sword@163.com>
2023-05-25 20:09:46 +0800

    调整Animate类动画，修改刷新率，适应高刷新率机器

--- src/lib/Animate.ts
+++ src/lib/Animate.ts
@@ -1,20 +1,39 @@
+import { el } from "element-plus/es/locale";
 import BaseCanvas from "./BaseCanvas";
 
 export default class Animate extends BaseCanvas {
 
+  private fps: number = 0;
+  private fpsInterval: number = 0;
+  private last: number = 0;
+
   constructor() {
     super();
+    this.fps = 60;
+    this.fpsInterval = 1000 / this.fps;
+    this.last= new Date().getTime();
   }
 
-  run(): void {
+  animate(): void {
     this.update();
     this.draw();
     if (this.showMark) {
       this.markCanvas.draw(this.context);
     }
-    requestAnimationFrame(this.run.bind(this));
+    // requestAnimationFrame(this.run.bind(this));
   }
   update(): void { }
   draw(): void { }
+
+  run() {
+    requestAnimationFrame(this.run.bind(this));
+
+    const now = new Date().getTime();
+    const elapsed = now - this.last;
+    if (elapsed > this.fpsInterval) {
+      this.last = now - (elapsed % this.fpsInterval);
+      this.animate();
+    }
+  }
 
 }
