dd37f3453785000749ef6fe8db7c467e8771f93b
ShenJinXiang <sjx-sword@163.com>
2023-10-07 12:28:19 +0800

    d

--- src/components/page/animation/RotateConstructionAnimation6/index.ts
+++ src/components/page/animation/RotateConstructionAnimation6/index.ts
@@ -1,11 +1,15 @@
 import Animate from "@/lib/Animate";
 
+const PI = Math.PI / 360;
+
 interface IOption {
     backgroundColor: string;
     elementColor: string;
     lineColor: string;
     baseRadiusRatio: number; // 基础半径
+    baseAngleStep: number; // 以及旋转角速度
     eleRadiusRatio: number;  // 旋转元素半径
+    eleAngleStep: number; // 以及旋转角速度
 };
 
 class Element {
@@ -17,8 +21,12 @@ export default class RotateConstructionAnimation extends Animate {
         elementColor: 'red',
         lineColor: 'rgba(255, 255, 255, 0.3)',
         baseRadiusRatio: 0.25,
-        eleRadiusRatio: 0.2
+        baseAngleStep: PI * 10,
+        eleRadiusRatio: 0.2,
+        eleAngleStep: PI * 5
     };
+    private baseRadius: number = 0;
+    private eleRadius: number = 0;
     constructor(width: number, height: number) {
         super();
         this.initRect(width, height);
@@ -26,6 +34,9 @@ export default class RotateConstructionAnimation extends Animate {
     }
 
     initData() {
+        const base = Math.min(this.width, this.height);
+        this.baseRadius = base * this.option.baseRadiusRatio;
+        this.eleRadius = base * this.option.eleRadiusRatio;
     }
 
     update() {
