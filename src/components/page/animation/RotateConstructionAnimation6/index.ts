d275fb483cdb3ba59aabeee8f1a9c6b8bc9da6e7
ShenJinXiang <sjx-sword@163.com>
2023-10-10 19:22:10 +0800

    RotateConstructionAnimation6: 轨迹

--- src/components/page/animation/RotateConstructionAnimation6/index.ts
+++ src/components/page/animation/RotateConstructionAnimation6/index.ts
@@ -18,19 +18,21 @@ class Element {
     private radius: number;
     private angle: number;
     private angleStep: number;
-    private lineColor: string;
     private point: Point;
-    constructor(radius: number, angle: number, angleStep: number, lineColor: string) {
+    private hasTrace: boolean;
+    private traces: Point[];
+    constructor(radius: number, angle: number, angleStep: number, hasTrace: boolean) {
         this.radius = radius;
         this.angle = angle;
         this.angleStep = angleStep;
-        this.lineColor = lineColor;
+        this.hasTrace = hasTrace;
         this.point = { x: 0, y: radius };
+        this.traces = [];
     }
     update() {
         this.angle += this.angleStep;
     }
-    draw(context: CanvasRenderingContext2D | null, origin: Point | null) {
+    draw(context: CanvasRenderingContext2D | null, origin: Point | null, lineColor: string) {
         if (!context) {
             return;
         }
@@ -41,14 +43,32 @@ class Element {
             x: this.radius * Math.cos(this.angle),
             y: this.radius * Math.sin(this.angle)
         }
+        if (this.hasTrace) {
+            this.traces.push({
+                x: origin.x + this.point.x,
+                y: origin.y + this.point.y
+            });
+        }
         context.save();
         context.translate(origin.x, origin.y);
         context.beginPath();
-        context.strokeStyle = this.lineColor;
+        context.strokeStyle = lineColor;
         context.lineWidth = 1;
         context.moveTo(0, 0);
         context.lineTo(this.point.x, this.point.y);
         context.closePath();
+        context.stroke();
+        context.restore();
+    }
+    public drawTraces(context: CanvasRenderingContext2D | null, color: string) {
+        if (!context || !this.hasTrace) {
+            return;
+        }
+        context.save();
+        context.beginPath();
+        context.strokeStyle = color;
+        context.lineWidth = 1;
+        this.traces.forEach((item) => context.lineTo(item.x, item.y));
         context.stroke();
         context.restore();
     }
@@ -61,12 +81,12 @@ export default class RotateConstructionAnimation extends Animate {
     private option: IOption = {
         backgroundColor: '#000',
         elementColor: 'red',
-        // lineColor: 'rgba(255, 255, 255, 0.3)',
-        lineColor: 'rgba(255, 0, 0, 1)',
-        baseRadiusRatio: 0.25,
+        lineColor: 'rgba(255, 255, 255, 0.3)',
+        // lineColor: 'rgba(255, 0, 0, 1)',
+        baseRadiusRatio: 0.35,
         baseAngleStep: PI / 180,
-        eleRadiusRatio: 0.2,
-        eleAngleStep: PI / 90,
+        eleRadiusRatio: 0.1,
+        eleAngleStep: PI / 20,
         elementNumber: 3
     };
     private baseAngle: number = 0;
@@ -83,13 +103,12 @@ export default class RotateConstructionAnimation extends Animate {
         const base = Math.min(this.width, this.height);
         const baseRadius = base * this.option.baseRadiusRatio;
         const eleRadius = base * this.option.eleRadiusRatio;
-        this.baseElement = new Element(baseRadius, 0, this.option.baseAngleStep, this.option.lineColor);
+        this.baseElement = new Element(baseRadius, 0, this.option.baseAngleStep, false);
         this.elements = [];
         const angleStep = 2 * PI / this.option.elementNumber;
         for (let i = 0; i < this.option.elementNumber; i++) {
-            this.elements.push(new Element(eleRadius, i * angleStep, this.option.eleAngleStep, this.option.lineColor));
+            this.elements.push(new Element(eleRadius, i * angleStep, this.option.eleAngleStep, true));
         }
-        console.log(this.elements);
     }
 
     update() {
@@ -108,9 +127,10 @@ export default class RotateConstructionAnimation extends Animate {
         this.context.save();
         this.context.translate(this.width / 2, this.height / 2);
         if (this.baseElement) {
-            this.baseElement.draw(this.context, { x: 0, y: 0 });
+            this.baseElement.draw(this.context, { x: 0, y: 0 }, this.option.lineColor);
             const p = this.baseElement.getPoint();
-            this.elements.forEach((item) => item.draw(this.context, p));
+            this.elements.forEach((item) => item.draw(this.context, p, this.option.lineColor));
+            this.elements.forEach((item) => item.drawTraces(this.context, this.option.elementColor));
         }
         this.context.restore();
     }
