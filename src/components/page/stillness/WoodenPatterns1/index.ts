d283c0855ac15283850139715b67048a9f21a8bd
ShenJinXiang <sjx-sword@163.com>
2024-02-17 20:52:52 +0800

    d

--- src/components/page/stillness/WoodenPatterns1/index.ts
+++ src/components/page/stillness/WoodenPatterns1/index.ts
@@ -4,18 +4,21 @@ import { pa } from "element-plus/es/locale";
 interface IOption {
     backgroundColor: string;
     gridWidth: number;
+    strokeColor: string;
 };
 
 export default class WoodenPatterns extends BaseCanvas {
     private option: IOption = {
-        backgroundColor: '#f1f1f1',
-        gridWidth: 200
+        backgroundColor: '#fadc39',
+        gridWidth: 200,
+        strokeColor: '#333'
     };
     gridCanvas: HTMLCanvasElement = document.createElement('canvas');
     gridContext: CanvasRenderingContext2D | null  = null;
     constructor(width: number, height: number) {
         super();
         this.initRect(width, height);
+        this.markCanvas.setStyle('rgba(60, 60, 204, 0.5)');
         this.initData();
     }
 
@@ -36,7 +39,7 @@ export default class WoodenPatterns extends BaseCanvas {
         const gw = gridWidth / 6;
         this.gridContext.save();
         this.gridContext.lineWidth = lineWidth;
-        this.gridContext.strokeStyle = 'black';
+        this.gridContext.strokeStyle = this.option.strokeColor;
         this.gridContext.translate(0.5 * gridWidth, 0.5 * gridWidth);
         this.gridContext.strokeRect(-0.5 * gw, -0.5 * gw, gw, gw);
         this.gridContext.strokeRect(-2 * gw, -2 * gw, 4 * gw, 4 * gw);
--- src/lib/MarkCanvas.ts
+++ src/lib/MarkCanvas.ts
@@ -1,19 +1,32 @@
+import { fi } from "element-plus/es/locale";
+
 export default class MarkCanvas {
   canvas: HTMLCanvasElement;
+  context: CanvasRenderingContext2D | null = null;
+  fillStyle: string;
 
   constructor(fillStyle: string, width: number, height: number) {
     this.canvas = document.createElement('canvas');
     this.canvas.width = width || 240;
     this.canvas.height = height || 60;
-    const context: CanvasRenderingContext2D | null = this.canvas.getContext('2d');
+    this.context = this.canvas.getContext('2d');
+    this.fillStyle = fillStyle;
+    this.init();
 
-    if (context) {
-      context.fillStyle = fillStyle;
-      context.textAlign = 'center';
-      context.textBaseline = 'middle';
-      context.font = `${this.canvas.height * 0.4}px cursive`;
-      context.fillText('shenjinxiang.com', this.canvas.width / 2, this.canvas.height / 2, 0.8 * this.canvas.width);
+  }
+  init () {
+    if (this.context) {
+      this.context.clearRect(0, 0, this.canvas.width, this.canvas.height);
+      this.context.fillStyle = this.fillStyle;
+      this.context.textAlign = 'center';
+      this.context.textBaseline = 'middle';
+      this.context.font = `${this.canvas.height * 0.4}px cursive`;
+      this.context.fillText('shenjinxiang.com', this.canvas.width / 2, this.canvas.height / 2, 0.8 * this.canvas.width);
     }
+  }
+  setStyle(fillStyle: string) {
+    this.fillStyle = fillStyle;
+    this.init();
   }
   draw(context: CanvasRenderingContext2D | null) {
     if (!context || !this.canvas) {
