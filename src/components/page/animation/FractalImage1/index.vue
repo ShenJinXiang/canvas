bb9594dce5411d4d24413f1a79ab727316f46c66
ShenJinXiang <sjx-sword@163.com>
2024-02-20 22:03:51 +0800

    d

---
+++ src/components/page/animation/FractalImage1/index.vue
@@ -0,0 +1,8 @@
+<template>
+    <div>
+      <canvas ref="canvasRef"></canvas>
+    </div>
+  </template>
+  <script lang="ts" setup>
+  import { onMounted, ref, Ref, watch } from 'vue';
+  </script>
--- src/views/page/animation/FractalImage1.vue
+++ src/views/page/animation/FractalImage1.vue
@@ -1,10 +1,12 @@
 <template>
     <div>
         FractalImage1
+        <fractal-image></fractal-image>
     </div>
   </template>
   <script lang="ts" setup>
   import { useWindowSize } from '@vueuse/core';
+  import FractalImage from '@/components/page/animation/FractalImage1/index.vue';
   
   const { width, height } = useWindowSize();
   </script>
