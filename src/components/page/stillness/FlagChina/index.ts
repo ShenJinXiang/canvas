59ecbf83c21b2f4fba8c658cf7d4c9880bae973c
ShenJinXiang <sjx-sword@163.com>
2024-02-23 22:22:56 +0800

    调整FlagChina代码

--- src/components/page/stillness/FlagChina/index.ts
+++ src/components/page/stillness/FlagChina/index.ts
@@ -8,63 +8,64 @@ interface Iconfig {
   lineColor: string,
 }
 export default class FlagChina extends BaseCanvas {
-  private config: Iconfig;
-  private showLines: boolean;
-  private stars: Star[] = [];
-  private lines: Line[] = [];
-  constructor(width: number, showLines: boolean = false) {
-    super();
-    this.width = width;
-    this.height = (this.width * 2) / 3;
-    this.showLines = showLines;
-    this.config = {
+  private static readonly config: Iconfig = {
       flagColor: '#F00',
       starColor: '#FF0',
       lineColor: '#fff',
-    };
+  };
+  private showLines: boolean;
+  private stars: Star[] = [];
+  private lines: Line[] = [];
+
+  constructor(width: number, showLines: boolean = false) {
+    super();
+    this.width = width;
+    this.showLines = showLines;
     this.canvas = null;
     this.context = null;
     this.initData();
   }
 
   private initData() {
-    this.height = (this.width * 2) / 3;
+    this.height = this.calculateHeight();
+    this.stars = this.createStarts();
+    this.lines = this.createLines();
+  }
+
+  private createStarts(): Star[] {
     const gridWidth = this.width / 30;
-    this.stars = [
+    return [
       new Star(5 * gridWidth, 5 * gridWidth, 3 * gridWidth, -Math.PI / 2),
       new Star(10 * gridWidth, 2 * gridWidth, 1 * gridWidth, Math.PI - Math.atan(3 / 5)),
       new Star(12 * gridWidth, 4 * gridWidth, 1 * gridWidth, Math.PI - Math.atan(1 / 7)),
       new Star(12 * gridWidth, 7 * gridWidth, 1 * gridWidth, Math.PI + Math.atan(2 / 7)),
       new Star(10 * gridWidth, 9 * gridWidth, 1 * gridWidth, Math.PI + Math.atan(4 / 5)),
     ];
-    this.lines = [];
-    this.lines.push(new Line(this.width / 2, 0, this.width / 2, this.height));
-    this.lines.push(new Line(0, this.height / 2, this.width, this.height / 2));
-    for (let index = 1; index < 15; index += 1) {
-      this.lines.push(new Line(index * gridWidth, 0, index * gridWidth, 10 * gridWidth));
-      if (index < 10) {
-        this.lines.push(new Line(0, index * gridWidth, 15 * gridWidth, index * gridWidth));
-      }
-      if (index < 5) {
-        this.lines.push(new Line(
-          this.stars[0].ox, this.stars[0].oy, this.stars[index].ox, this.stars[index].oy,
-        ));
-      }
-    }
+  }
+
+  private createLines(): Line[] {
+    const gridWidth = this.width / 30;
+    return [
+      new Line(this.width / 2, 0, this.width / 2, this.height),
+      new Line(0, this.height / 2, this.width, this.height / 2),
+      ...[1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14].map((i) => new Line(i * gridWidth, 0, i * gridWidth, 10 * gridWidth)),
+      ...[1, 2, 3, 4, 5, 6, 7, 8, 9].map((i) => new Line(0, i * gridWidth, 15 * gridWidth, i * gridWidth)),
+      ...this.stars.map((star, i) => new Line(star.ox, star.oy, this.stars[0].ox, this.stars[0].oy)),
+    ];
   }
   draw() {
     if (!this.canvas || !this.context) {
       return;
     }
     this.context.clearRect(0, 0, this.canvas.width, this.canvas.height);
-    this.context.fillStyle = this.config.flagColor;
+    this.context.fillStyle = FlagChina.config.flagColor;
     this.context.fillRect(0, 0, this.canvas.width, this.canvas.height);
     this.stars.forEach((item) => {
-      item.fill(this.context, { fillStyle: this.config.starColor });
+      item.fill(this.context, { fillStyle: FlagChina.config.starColor });
     });
     if (this.showLines) {
       this.lines.forEach((item) => {
-        item.stroke(this.context, { strokeStyle: this.config.lineColor, lineWidth: 1 });
+        item.stroke(this.context, { strokeStyle: FlagChina.config.lineColor, lineWidth: 1 });
       });
     }
   }
@@ -86,4 +87,7 @@ export default class FlagChina extends BaseCanvas {
     }
   }
 
+  private calculateHeight(): number {
+    return this.width * 2 / 3;
+  }
 }
