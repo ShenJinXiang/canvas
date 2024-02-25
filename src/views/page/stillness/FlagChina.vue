0cba2c0f54044c3d1df6f30e27cd2cfc403a8c6d
ShenJinXiang <sjx-sword@163.com>
2024-02-25 21:06:35 +0800

    调整代码

--- src/views/page/stillness/FlagChina.vue
+++ src/views/page/stillness/FlagChina.vue
@@ -16,12 +16,14 @@
 </template>
 <script lang="ts" setup>
 import FlagChina from "@/components/page/stillness/FlagChina/index.vue";
-import { ref, Ref } from "vue";
+import { ref, Ref, computed } from "vue";
 
 const showContro = ref(false);
 const direction = ref("rtl");
 const showLines: Ref<boolean> = ref(false);
 const width: Ref<number> = ref(900);
-const maxWidth: number = Math.min(window.innerWidth - 40, (window.innerHeight - 20) * 1.5 - 40);
+const maxWidth: Ref<number> = computed(() => {
+  return Math.min(window.innerWidth - 40, (window.innerHeight - 20) * 1.5 - 40);
+});
 const minWidth: number = 400;
 </script>
