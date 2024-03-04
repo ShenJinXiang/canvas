66c34027970017a7208d39da7d4c446969ef0fa7
ShenJinXiang <sjx-sword@163.com>
2024-03-04 21:53:37 +0800

    d

---
+++ src/components/page/background/RainAnimation/index.vue
@@ -0,0 +1,15 @@
+<template>
+    <div>下雨动画.canvas {{ props.width }} - {{ props.height }}</div>
+</template>
+<script lang="ts" setup>
+const props = defineProps({
+    width: {
+        type: Number,
+        required: true
+    },
+    height: {
+        type: Number,
+        required: true
+    }
+});
+</script>
--- src/views/page/background/RainAnimation.vue
+++ src/views/page/background/RainAnimation.vue
@@ -1,7 +1,9 @@
 <template>
-    <div>
-        下雨动画
-    </div>
+    <rain-animation :width="width" :height="height - 40"></rain-animation>
 </template>
-<script lang="ts">
-</script>"
+<script lang="ts" setup>
+import RainAnimation from '@/components/page/background/RainAnimation/index.vue';
+import { useWindowSize } from '@vueuse/core';
+
+const { width, height } = useWindowSize();
+</script>
