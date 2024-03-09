b868e69e7cac46a1b349b4df5b0eccbea717354b
ShenJinXiang <sjx-sword@163.com>
2024-03-09 22:11:50 +0800

    RainAnimation添加Element类

--- src/components/page/background/RainAnimation/index.ts
+++ src/components/page/background/RainAnimation/index.ts
@@ -6,6 +6,38 @@ interface IOption {
 }
 
 class Element {
+    private x: number;
+    private y: number;
+    private vx: number;
+    private vy: number;
+    private length: number;
+    constructor({x, y, vx, vy, length}: {x: number, y: number, vx: number, vy: number, length: number}) {
+        this.x = x;
+        this.y = y;
+        this.vx = vx;
+        this.vy = vy;
+        this.length = length;
+    }
+
+    update(maxX: number, maxY: number) {
+        this.x += this.vx;
+        this.y += this.vy;
+        if (this.x < 0 || this.x > maxX || this.y - this.length > maxY) {
+            this.x = Math.random() * maxX;
+            this.y = -this.length;
+        }
+    }
+
+    draw(context: CanvasRenderingContext2D | null, color: string) {
+        if (!context) {
+            return;
+        }
+        context.beginPath();
+        context.fillStyle = color;
+        context.lineCap = 'round';
+        context.moveTo(this.x, this.y);
+        context.lineTo(this.x + this.length * this.vx, this.y + this.length * this.vy);
+    }
 
 }
 
@@ -31,5 +63,10 @@ export default class RainAnimation extends Animate {
         }
         this.context.save();
         this.context.restore();
+    }
+
+    public setRect(width: number, height: number) {
+        this.initRect(width, height);
+        this.initData();
     }
 }
--- src/components/page/background/RainAnimation/index.vue
+++ src/components/page/background/RainAnimation/index.vue
@@ -1,7 +1,10 @@
 <template>
-    <div>下雨动画.canvas {{ props.width }} - {{ props.height }}</div>
+    <canvas ref="canvasRef"></canvas>
 </template>
 <script lang="ts" setup>
+import RainAnimation from './index';
+import { onMounted, ref, watch } from 'vue';
+
 const props = defineProps({
     width: {
         type: Number,
@@ -12,4 +15,26 @@ const props = defineProps({
         required: true
     }
 });
+
+
+const canvasRef = ref();
+
+const rainAnimation = new RainAnimation(props.width, props.height);
+
+onMounted(() => {
+    rainAnimation.initCanvas(canvasRef.value).run();
+});
+
+watch(
+  () => props.width,
+  () => {
+    rainAnimation.setRect(props.width, props.height);
+  }
+);
+watch(
+  () => props.height,
+  () => {
+    rainAnimation.setRect(props.width, props.height);
+  }
+);
 </script>
