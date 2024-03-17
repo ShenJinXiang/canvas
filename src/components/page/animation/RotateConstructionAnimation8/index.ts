436f6533c2df0c647e8c7603b11d63abb808f999
ShenJinXiang <sjx-sword@163.com>
2024-03-17 21:54:24 +0800

    df

--- src/components/page/animation/RotateConstructionAnimation8/index.ts
+++ src/components/page/animation/RotateConstructionAnimation8/index.ts
@@ -5,14 +5,15 @@ const PI = Math.PI;
 
 interface IOption {
   backgroundColor: string;
+  polygonNumber: number;
 }
-
-
 
 export default class RotateConstructionAnimation extends Animate {
   private static readonly OPTION: IOption = {
     backgroundColor: '#000',
+    polygonNumber: 40,
   }
+  private elements: Polygon[] = [];
   constructor(width: number, height: number) {
     super();
     this.initRect(width, height);
@@ -20,6 +21,16 @@ export default class RotateConstructionAnimation extends Animate {
   }
 
   initData() {
+    this.elements = [];
+    for (let i = 0; i < RotateConstructionAnimation.OPTION.polygonNumber; i++) {
+      this.elements.push(new Polygon({
+        ox: this.width / 2,
+        oy: this.height / 2,
+        radius: 140,
+        rotate: PI / 2,
+        sideNum: 5,
+      }));
+    }
   }
 
   draw() {
