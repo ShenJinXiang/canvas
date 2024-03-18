0515acbd934ccf82d6b97d8efa950f034276e9a1
ShenJinXiang <sjx-sword@163.com>
2024-03-18 21:45:00 +0800

    调整element

--- src/components/page/animation/RotateConstructionAnimation8/index.ts
+++ src/components/page/animation/RotateConstructionAnimation8/index.ts
@@ -1,5 +1,6 @@
 import Animate from "@/lib/Animate";
-import { Polygon } from "@/lib/Polygon";
+import { FillOption, StrokeOption } from "@/lib/DrawOption";
+import { Polygon, PolygonOption } from "@/lib/Polygon";
 
 const PI = Math.PI;
 
@@ -8,12 +9,35 @@ interface IOption {
   polygonNumber: number;
 }
 
+interface RotatePolygonOption extends PolygonOption {
+  style: string;
+}
+
+class RotatePolygon extends Polygon {
+
+  private style: string;
+
+  constructor(options: RotatePolygonOption) {
+    super(options);
+    this.style = options.style;
+  }
+
+  stroke(context: CanvasRenderingContext2D | null, { lineWidth, strokeStyle = this.style }: StrokeOption): void {
+      super.stroke(context, { lineWidth, strokeStyle });
+  }
+
+  fill(ctx: CanvasRenderingContext2D | null, { fillStyle = this.style }: FillOption): void {
+      super.fill(ctx, { fillStyle });
+  }
+
+}
+
 export default class RotateConstructionAnimation extends Animate {
   private static readonly OPTION: IOption = {
     backgroundColor: '#000',
     polygonNumber: 40,
   }
-  private elements: Polygon[] = [];
+  private elements: RotatePolygon[] = [];
   constructor(width: number, height: number) {
     super();
     this.initRect(width, height);
@@ -24,12 +48,13 @@ export default class RotateConstructionAnimation extends Animate {
     const radius = Math.min(this.width, this.height) * 0.45;
     this.elements = [];
     for (let i = 0; i < RotateConstructionAnimation.OPTION.polygonNumber; i++) {
-      this.elements.push(new Polygon({
+      this.elements.push(new RotatePolygon({
         ox: this.width / 2,
         oy: this.height / 2,
         radius: radius,
         rotate: PI / 2,
         sideNum: 5,
+        style: 'hsla(' + (i * 15) + ', 100%, 60%, 1)'
       }));
     }
   }
@@ -40,14 +65,7 @@ export default class RotateConstructionAnimation extends Animate {
     }
     this.clear(RotateConstructionAnimation.OPTION.backgroundColor);
     this.context.save();
-    const p = new Polygon({
-      ox: this.width / 2,
-      oy: this.height / 2,
-      radius: 140,
-      rotate: PI / 2,
-      sideNum: 5,
-    });
-    p.stroke(this.context, { lineWidth: 2, strokeStyle: 'red' });
+    this.elements.forEach((element) => element.stroke(this.context, { lineWidth: 1 }));
     this.context.restore();
   }
 
