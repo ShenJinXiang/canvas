28d2a88c435fc7f2f5bb7527c0f1a9589e2a3250
ShenJinXiang <sjx-sword@163.com>
2024-03-28 16:46:46 +0800

    调整element类的绘制方案

--- src/components/page/stillness/Background1/index.ts
+++ src/components/page/stillness/Background1/index.ts
@@ -9,13 +9,14 @@ class Element {
     private x: number;
     private y: number;
     private radius: number;
-    private rs: number[];
     private angles: number[];
+    private bl: number[];
     constructor() {
         this.x = 0;
         this.y = 0;
         this.radius = 0;
-        this.rs = [1, 0.9, 0.7, 0.5, 0.1]
+        // 最大半径， 淡色弧线半径，淡色弧线宽度， 四个小圆位置， 四个小圆半径, 四条线位置，四条线最宽的宽度
+        this.bl = [1, 0.8, 0.2, 0.5, 0.08, 0.1, 0.02]
         const angle = -Math.PI / 5;
         this.angles = [1 * angle, 2 * angle, 3 * angle, 4 * angle ]
     }
@@ -35,33 +36,33 @@ class Element {
         context.save();
         context.translate(this.x, this.y);
 
+        // 最大半圆
         context.beginPath();
         context.fillStyle = backgroundColor;
-        context.arc(0, 0, this.radius * this.rs[0], -Math.PI, 0, false);
+        context.arc(0, 0, this.radius * this.bl[0], -Math.PI, 0, false);
         context.fill();
         
+        // 半圆弧线
         context.beginPath();
-        context.fillStyle = style;
-        context.arc(0, 0, this.radius * this.rs[1], -Math.PI, 0, false);
-        context.fill();
-
-        context.beginPath();
-        context.fillStyle = backgroundColor;
-        context.arc(0, 0, this.radius * this.rs[2], -Math.PI, 0, false);
-        context.fill();
+        context.strokeStyle = style;
+        context.lineWidth = this.radius * this.bl[2];
+        context.arc(0, 0, this.radius * this.bl[1], -Math.PI, 0, false);
+        context.stroke();
 
         this.angles.forEach((angle: number, i: number) => {
+            // 小圆
             context.save();
             context.rotate(angle);
             context.fillStyle = style;
             context.beginPath();
-            context.arc(this.radius * this.rs[3], 0, this.radius * 0.08, 0, 2 * Math.PI, false);
+            context.arc(this.radius * this.bl[3], 0, this.radius * this.bl[4], 0, 2 * Math.PI, false);
             context.fill();
 
+            // 线
             context.beginPath();
-            context.moveTo(this.radius * this.rs[4], 0);
-            context.lineTo(this.radius * this.rs[3], this.radius * 0.02);
-            context.lineTo(this.radius * this.rs[3], -this.radius * 0.02);
+            context.moveTo(this.radius * this.bl[5], 0);
+            context.lineTo(this.radius * this.bl[3], this.radius * this.bl[6]);
+            context.lineTo(this.radius * this.bl[3], -this.radius * this.bl[6]);
             context.closePath();
             context.fill();
             context.restore();
