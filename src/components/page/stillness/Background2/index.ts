7a2a524868dd6ca62d4950290cfd033d73d8252a
ShenJinXiang <sjx-sword@163.com>
2024-04-07 21:35:15 +0800

    背景图2调整element

--- src/components/page/stillness/Background2/index.ts
+++ src/components/page/stillness/Background2/index.ts
@@ -1,8 +1,17 @@
 import BaseCanvas from "@/lib/BaseCanvas";
 
+interface IOption {
+    backgroundColor: string;
+    showColor: string;
+}
+
 class Element {
+    private x: number;
+    private y: number;
     private size: number;
-    constructor(size: number) {
+    constructor(x: number, y: number, size: number) {
+        this.x = x;
+        this.y = y;
         this.size = size;
     }
     draw(context: CanvasRenderingContext2D | null) {
@@ -10,17 +19,32 @@ class Element {
             return;
         }
         context.save();
-        context.translate(this.size / 2, this.size / 2);
-        context.fillStyle = '#8b0903';
-        context.fillRect(-this.size / 4, -this.size / 4, this.size / 2, this.size / 2);
+        // context.translate(this.x, this.y);
+        context.translate(this.x + this.size / 2, this.y + this.size / 2);
+        context.fillStyle = '#f8c687';
+        // context.fillRect(-this.size / 4, -this.size / 4, this.size / 2, this.size / 2);
+        context.fillRect(-this.size / 2, -this.size / 2, this.size, this.size);
         context.restore();
     }
 }
 
 export default class Background extends BaseCanvas {
+    private static readonly option: IOption = {
+        backgroundColor: '#8b0903',
+        showColor: '#f8c687',
+    };
+    private elements: Element[] = [];
     constructor(width: number, height: number) {
         super();
         this.initRect(width, height);
+        this.initData();
+    }
+
+    initData() {
+        this.elements = [];
+        for (let i = 0; i < 10; i++) {
+            this.elements.push(new Element(this.width / 2 - this.width / 20, this.height / 2 - this.width / 20, this.width / 10));
+        }
     }
 
     draw(): void {
@@ -28,10 +52,10 @@ export default class Background extends BaseCanvas {
         if (!this.context) {
             return;
         }
-        // this.context.fillStyle = '#8b0903';
-        // this.context.fillRect(100, 100, 300, 220);
-
-        // this.context.fillStyle = '#f8c687';
-        // this.context.fillRect(600, 600, 100, 60);
+        this.clear(Background.option.backgroundColor);
+        // this.clear();
+        this.elements.forEach((element) => {
+            element.draw(this.context);
+        });
     }
 }
