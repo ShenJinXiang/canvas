1c811645a40676ca576179e48ad8273f4284dba8
ShenJinXiang <sjx-sword@163.com>
2024-04-07 18:25:02 +0800

    开始编写element类

--- src/components/page/stillness/Background2/index.ts
+++ src/components/page/stillness/Background2/index.ts
@@ -1,5 +1,22 @@
 import BaseCanvas from "@/lib/BaseCanvas";
 
+class Element {
+    private size: number;
+    constructor(size: number) {
+        this.size = size;
+    }
+    draw(context: CanvasRenderingContext2D | null) {
+        if (!context) {
+            return;
+        }
+        context.save();
+        context.translate(this.size / 2, this.size / 2);
+        context.fillStyle = '#8b0903';
+        context.fillRect(-this.size / 4, -this.size / 4, this.size / 2, this.size / 2);
+        context.restore();
+    }
+}
+
 export default class Background extends BaseCanvas {
     constructor(width: number, height: number) {
         super();
@@ -11,10 +28,10 @@ export default class Background extends BaseCanvas {
         if (!this.context) {
             return;
         }
-        this.context.fillStyle = '#8b0903';
-        this.context.fillRect(100, 100, 300, 220);
+        // this.context.fillStyle = '#8b0903';
+        // this.context.fillRect(100, 100, 300, 220);
 
-        this.context.fillStyle = '#f8c687';
-        this.context.fillRect(600, 600, 100, 60);
+        // this.context.fillStyle = '#f8c687';
+        // this.context.fillRect(600, 600, 100, 60);
     }
 }
