2233faf8a8ad19de248ecb1e5e90ad62842e769b
ShenJinXiang <sjx-sword@163.com>
2024-04-23 09:41:01 +0800

    分形图2 设置Element类基本绘制

--- src/components/page/animation/FractalImage2/index.ts
+++ src/components/page/animation/FractalImage2/index.ts
@@ -1,20 +1,73 @@
 import Animate from "@/lib/Animate";
+import { StrokeOption } from "@/lib/DrawOption";
 
 interface IOption {
   backgroundColor: string;
+  lineWidth: number;
+  lineColor: string;
+}
+
+class Element {
+  private x: number;
+  private y: number;
+  private len: number;
+  private angle: number;
+  private current: number;
+  private time: number;
+  private lenStep: number;
+  constructor(x: number, y: number, len: number, angle: number, time: number) {
+    this.x = x;
+    this.y = y;
+    this.len = len;
+    this.angle = angle;
+    this.time = time;
+    this.current = 0;
+    this.lenStep = this.len / (2 * this.time);
+  }
+
+  update() {
+    if (this.current < this.time) {
+      this.current += 1;
+    }
+  }
+
+  draw(context: CanvasRenderingContext2D|null, { lineWidth = 1, strokeStyle = 'red' }: StrokeOption) {
+    if (!context) {
+      return;
+    }
+    context.save();
+    context.lineWidth = lineWidth;
+    context.strokeStyle = strokeStyle;
+    context.translate(this.x, this.y);
+    context.rotate(this.angle);
+    context.beginPath();
+    context.moveTo(-this.lenStep * this.current, 0);
+    context.lineTo(this.lenStep * this.current, 0);
+    context.stroke();
+    context.restore();
+  }
 }
 
 export default class FractalImage extends Animate {
   private static readonly OPTION: IOption = {
     backgroundColor: '#f1f1f1',
+    lineWidth: 1,
+    lineColor: '#0075c9'
   }
+  private element: Element|null = null;
   constructor(width: number, height: number) {
     super();
     this.initRect(width, height);
     this.initData();
   }
 
-  initData() {}
+  initData() {
+    this.element = new Element(this.width / 2, this.height / 2, 400, 0, 100);
+  }
+
+  update() {
+    this.element?.update();
+  }
 
   draw(): void {
     if (!this.context) {
@@ -22,8 +75,12 @@ export default class FractalImage extends Animate {
     }
     this.clear(FractalImage.OPTION.backgroundColor);
     this.context.save();
-    this.context.fillStyle = 'red';
-    this.context.fillRect(100, 100, 300, 220);
+    if (this.element) {
+      this.element.draw(this.context, {
+        lineWidth: FractalImage.OPTION.lineWidth,
+        strokeStyle: FractalImage.OPTION.lineColor
+      });
+    }
     this.context.restore();
   }
 
