a27ff098c49159fe71ace77c17037eae2a559594
ShenJinXiang <sjx-sword@163.com>
2024-04-24 16:36:08 +0800

    分形图2动画

--- src/components/page/animation/FractalImage2/index.ts
+++ src/components/page/animation/FractalImage2/index.ts
@@ -5,6 +5,9 @@ interface IOption {
   backgroundColor: string;
   lineWidth: number;
   lineColor: string;
+  ratio: number;
+  deepNum: number;
+  time: number;
 }
 
 class Element {
@@ -31,7 +34,7 @@ class Element {
     }
   }
 
-  draw(context: CanvasRenderingContext2D|null, { lineWidth = 1, strokeStyle = 'red' }: StrokeOption) {
+  draw(context: CanvasRenderingContext2D | null, { lineWidth = 1, strokeStyle = 'red' }: StrokeOption) {
     if (!context) {
       return;
     }
@@ -46,15 +49,50 @@ class Element {
     context.stroke();
     context.restore();
   }
+
+  setCurrent(current: number) {
+    this.current = current;
+  }
+
+  setComplete() {
+    this.current = this.time;
+  }
+
+  children(ratio: number): Element[] {
+    const len = this.len * ratio;
+    const angle = this.angle + Math.PI / 2;
+    return [
+      new Element(
+        this.x + 0.5 * this.len * Math.cos(this.angle),
+        this.y + 0.5 * this.len * Math.sin(this.angle),
+        len,
+        angle,
+        this.time
+      ),
+      new Element(
+        this.x - 0.5 * this.len * Math.cos(this.angle),
+        this.y - 0.5 * this.len * Math.sin(this.angle),
+        len,
+        angle,
+        this.time
+      )
+    ]
+
+  }
 }
 
 export default class FractalImage extends Animate {
   private static readonly OPTION: IOption = {
     backgroundColor: '#f1f1f1',
     lineWidth: 1,
-    lineColor: '#0075c9'
+    lineColor: '#0075c9',
+    ratio: 0.7,
+    deepNum: 13,
+    time: 50
   }
-  private element: Element|null = null;
+  private elementGroup: Element[][] = [];
+  private currentDeep: number = 0;
+  private currentTime: number = 0;
   constructor(width: number, height: number) {
     super();
     this.initRect(width, height);
@@ -62,25 +100,61 @@ export default class FractalImage extends Animate {
   }
 
   initData() {
-    this.element = new Element(this.width / 2, this.height / 2, 400, 0, 100);
+    this.currentDeep = 0;
+    this.currentTime = 0;
+    // this.element = new Element(this.width / 2, this.height / 2, 400, 0, 100);
+    this.initElementGroup();
+  }
+
+  initElementGroup() {
+    this.elementGroup = [];
+    for (let i = 0; i < FractalImage.OPTION.deepNum; i++) {
+      const eles: Element[] = [];
+      if (i === 0) {
+        eles.push(new Element(this.width / 2, this.height / 2, this.width / 2.5, 0, FractalImage.OPTION.time));
+      } else {
+        this.elementGroup[i - 1].forEach(item => eles.push(...item.children(FractalImage.OPTION.ratio)));
+      }
+      this.elementGroup.push(eles);
+    }
   }
 
   update() {
-    this.element?.update();
+    this.elementGroup.forEach((item, index) => {
+      if (this.currentDeep === index) {
+        item.forEach(ele => ele.update());
+      }
+      if (this.currentDeep > index) {
+        item.forEach(ele => ele.setComplete());
+      }
+    });
+    this.currentTime++;
+    if (this.currentTime >= FractalImage.OPTION.time) {
+      this.currentTime = 0;
+      this.currentDeep++;
+      if (this.currentDeep >= FractalImage.OPTION.deepNum) {
+        this.currentDeep = 0;
+        this.elementGroup.forEach((item) => {
+          item.forEach(ele => ele.setCurrent(0))
+        })
+      }
+    }
   }
 
   draw(): void {
     if (!this.context) {
-        return;
+      return;
     }
     this.clear(FractalImage.OPTION.backgroundColor);
     this.context.save();
-    if (this.element) {
-      this.element.draw(this.context, {
-        lineWidth: FractalImage.OPTION.lineWidth,
-        strokeStyle: FractalImage.OPTION.lineColor
-      });
-    }
+    this.elementGroup.forEach((item, index) => {
+      if (this.currentDeep >= index) {
+        item.forEach(ele => ele.draw(this.context, {
+          lineWidth: FractalImage.OPTION.lineWidth,
+          strokeStyle: FractalImage.OPTION.lineColor
+        }));
+      }
+    });
     this.context.restore();
   }
 
