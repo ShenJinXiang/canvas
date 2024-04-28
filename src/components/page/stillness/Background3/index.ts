59fbc02bcff412dfc3daceb3de1d4981922d9780
ShenJinXiang <sjx-sword@163.com>
2024-04-28 18:00:14 +0800

    背景3 添加Element类的draw，绘制基本j图案

--- src/components/page/stillness/Background3/index.ts
+++ src/components/page/stillness/Background3/index.ts
@@ -10,10 +10,12 @@ class Element {
   private x: number;
   private y: number;
   private size: number;
+  private p: number;
   constructor(x: number, y: number, size: number) {
     this.x = x;
     this.y = y;
     this.size = size;
+    this.p = this.size / 24;
   }
   draw(context: CanvasRenderingContext2D | null, showColor: string) {
     if (!context) {
@@ -22,9 +24,20 @@ class Element {
     const lineWidth = 10;
     context.save();
     context.translate(this.x, this.y);
-    context.lineWidth = lineWidth;
+    // context.lineWidth = lineWidth;
     context.strokeStyle = showColor;
-    context.strokeRect(-0.5 * this.size + 0.5 * lineWidth, -0.5 * this.size + 0.5 * lineWidth, this.size - lineWidth, this.size - lineWidth);
+    // context.strokeRect(-0.5 * this.size + 0.5 * lineWidth, -0.5 * this.size + 0.5 * lineWidth, this.size - lineWidth, this.size - lineWidth);
+    context.lineWidth = this.p;
+
+    for (let i = 0; i < 6; i++) {
+      const s = (i * 2 + 0.5) * this.p;
+      context.strokeRect(-s, -s, 2 * s, 2 * s);
+    }
+    // context.strokeRect(-2.5 * this.p, -2.5 * this.p, 5 * this.p, 5 * this.p);
+
+    // context.strokeStyle = 'red';
+    // context.lineWidth = 1;
+    // context.strokeRect(-0.5 * this.size, -0.5 * this.size, this.size, this.size);
     context.restore();
   }
 }
@@ -52,7 +65,7 @@ export default class Background extends BaseCanvas {
     if (!this.context) {
       return;
     }
-    this.clear();
+    this.clear(Background.OPTION.backgroundColor);
     this.context.save();
     this.elements.forEach((item) => item.draw(this.context, Background.OPTION.showColor));
     this.context.restore();
