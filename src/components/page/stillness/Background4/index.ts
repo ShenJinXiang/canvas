bce8a9e8f15dbc2a63193b49d0f5c8c234704647
ShenJinXiang <sjx-sword@163.com>
2024-05-13 21:31:56 +0800

    start background4 element

--- src/components/page/stillness/Background4/index.ts
+++ src/components/page/stillness/Background4/index.ts
@@ -10,14 +10,34 @@ class Element {
   private x: number;
   private y: number;
   private size: number;
+  private p: number;
   constructor(x: number, y: number, size: number) {
     this.x = x;
     this.y = y;
     this.size = size;
+    this.p = this.size / 7.5;
+  }
+  draw(context: CanvasRenderingContext2D | null, showColor: string) {
+    if (!context) {
+      return;
+    }
+    context.save();
+    context.translate(this.x, this.y);
+    context.beginPath();
+    context.fillStyle = showColor;
+    context.rotate(Math.PI / 4);
+
+    context.strokeStyle = showColor;
+    context.strokeRect(-0.5 * this.size, -0.5 * this.size, this.size, this.size);
+    context.moveTo(0, 0);
+    context.lineTo(this.size / 2, this.size / 2);
+    context.stroke();
+    context.restore();
   }
 }
 
 export default class Background extends BaseCanvas {
+  private elements: Element[] = [];
   private static readonly OPTION: IOption = {
     backgroundColor: '#000',
     showColor: '#f1f1f1',
@@ -30,6 +50,9 @@ export default class Background extends BaseCanvas {
   }
 
   initData() {
+    this.elements = [
+      new Element(0.5 * this.width, 0.5 * this.height, 100),
+    ];
   }
 
   draw() {
@@ -38,10 +61,9 @@ export default class Background extends BaseCanvas {
     }
     this.clear(Background.OPTION.backgroundColor);
     this.context.save();
-    this.context.fillStyle = Background.OPTION.showColor;
-    this.context.fillRect(100, 100, 300, 220);
-    this.drawMark();
+    this.elements.forEach(element => element.draw(this.context, Background.OPTION.showColor));
     this.context.restore();
+    this.drawMark();
   }
 
   public setRect(width: number, height: number) {
