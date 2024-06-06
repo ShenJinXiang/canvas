43ecf07c5b0ca2258712057e0093e8ab825d7467
ShenJinXiang <sjx-sword@163.com>
2024-06-06 22:05:40 +0800

    完善Background5 中Element绘制方法

--- src/components/page/stillness/Background5/index.ts
+++ src/components/page/stillness/Background5/index.ts
@@ -1,4 +1,6 @@
 import BaseCanvas from "@/lib/BaseCanvas";
+import { StrokeOption } from "@/lib/DrawOption";
+import Point from "@/lib/Point";
 
 interface IOption {
     backgroundColor: string;
@@ -12,24 +14,46 @@ class Element {
     private rotate: number;
     private size: number;
     private l: number;
+    private points: Point[] = [];
     constructor(x: number, y: number, size: number, rotate: number) {
         this.x = x;
         this.y = y;
         this.size = size;
-        this.rotate = size;
+        this.rotate = rotate;
         this.l = 0.5 * this.size;
+        this.initPoints();
+    }
+    private initPoints() {
+        this.points.push({x: this.x, y: this.y});
+        this.points.push({x: this.x - this.l * Math.cos(this.rotate), y: this.y + this.l * Math.sin(this.rotate)});
+        this.points.push({x: this.x + this.l * Math.cos(this.rotate), y: this.y + this.l * Math.sin(this.rotate)});
+        this.points.push({x: this.points[0].x, y: this.points[0].y + this.l * 0.2});
+        this.points.push({x: this.points[1].x, y: this.points[1].y + this.l * 0.2});
+        this.points.push({x: this.points[2].x, y: this.points[2].y + this.l * 0.2});
+    }
+    private joinLines(context: CanvasRenderingContext2D, points: Point[], strokeStyle: string = '#fff', lineWidth: number = 1) {
+        context.beginPath();
+        context.strokeStyle = strokeStyle;
+        context.lineWidth = lineWidth;
+        points.forEach(p => context.lineTo(p.x, p.y));
+        context.stroke();
     }
     draw(context: CanvasRenderingContext2D | null, showColor: string) {
         if (!context) {
             return;
         }
         context.save();
-        context.translate(this.x, this.y);
-        context.strokeStyle = showColor;
-        context.moveTo(-this.l * Math.cos(this.rotate), this.l * Math.sin(this.rotate));
-        context.lineTo(0, 0);
-        context.lineTo(this.l * Math.cos(this.rotate), this.l * Math.sin(this.rotate));
-        context.stroke();
+        this.joinLines(context, [this.points[1], this.points[0], this.points[2]]);
+        this.joinLines(context, [this.points[4], this.points[3], this.points[5]]);
+        this.joinLines(context, [this.points[0], this.points[3]]);
+        this.joinLines(context, [this.points[1], this.points[4]]);
+        this.joinLines(context, [this.points[2], this.points[5]]);
+        // context.translate(this.x, this.y);
+        // context.strokeStyle = showColor;
+        // context.moveTo(-this.l * Math.cos(this.rotate), this.l * Math.sin(this.rotate));
+        // context.lineTo(0, 0);
+        // context.lineTo(this.l * Math.cos(this.rotate), this.l * Math.sin(this.rotate));
+        // context.stroke();
         context.restore();
     }
 }
