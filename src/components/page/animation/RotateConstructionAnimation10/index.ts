f3d22bd5503648a0805909b7b545dbc946001768
ShenJinXiang <sjx-sword@163.com>
2024-07-13 22:13:38 +0800

    d

--- src/components/page/animation/RotateConstructionAnimation10/index.ts
+++ src/components/page/animation/RotateConstructionAnimation10/index.ts
@@ -1,4 +1,5 @@
 import Animate from "@/lib/Animate";
+import Point from "@/lib/Point";
 
 interface IOption {
     backgroundColor: string;
@@ -11,12 +12,50 @@ const OPTION: IOption = {
 }
 
 class Element {
-    constructor() {
+    private txt: string;
+    private angle: number = 0;
+    private radius: number = 0;
+    private angleStep: number;
+    private counterclockwise: boolean;
+    private point: Point;
+    constructor(txt: string, radius: number, angleStep: number, counterclockwise: boolean = false) {
+        this.txt = txt;
+        this.radius = radius;
+        this.angle = 0;
+        this.angleStep = angleStep;
+        this.counterclockwise = counterclockwise;
+        this.point = this.position();
+    }
 
+    reset() {
+        this.angle = 0;
+        this.point = this.position();
+    }
+
+    position(): Point {
+        return {
+            x: this.radius * Math.cos(this.angle),
+            y: this.radius * Math.sin(this.angle)
+        }
+
+    }
+
+    draw(context: CanvasRenderingContext2D | null) {
+        if (!context) {
+            return;
+        }
+        context.save();
+        context.beginPath();
+        context.strokeStyle = OPTION.showColor;
+        context.moveTo(this.p1.x, this.p1.y);
+        context.lineTo(this.p2.x, this.p2.y);
+        context.stroke();
+        context.restore();
     }
 }
 
 export default class RotateConstructionAnimation extends Animate {
+    private ele: Element = new Element();
     constructor(width: number, height: number) {
         super();
         this.initRect(width, height);
@@ -24,6 +63,7 @@ export default class RotateConstructionAnimation extends Animate {
     }
 
     private initData() {
+
 
     }
 
