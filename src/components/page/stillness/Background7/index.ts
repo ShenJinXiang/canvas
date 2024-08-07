0629f926bad6f5fd0d0e8e37e510496b05b342b6
ShenJinXiang <sjx-sword@163.com>
2024-08-07 10:50:39 +0800

    完成elements布局调整

--- src/components/page/stillness/Background7/index.ts
+++ src/components/page/stillness/Background7/index.ts
@@ -1,4 +1,5 @@
 import BaseCanvas from "@/lib/BaseCanvas";
+import Point from "@/lib/Point";
 
 interface IOption {
     backgroundColor: string;
@@ -10,19 +11,30 @@ class Element {
     private x: number;
     private y: number;
     private radius: number;
+    private points: Point[] = [];
     constructor(x: number, y: number, radius: number) {
         this.x = x;
         this.y = y;
         this.radius = radius;
+        const angleStep = Math.PI / 3;
+        const startAngle = -Math.PI / 2;
+        for (let i = 0; i < 6; i++) {
+            this.points.push({
+                x: this.radius * Math.cos(angleStep * i + startAngle),
+                y: this.radius * Math.sin(angleStep * i + startAngle)
+            });
+        }
     }
     draw(context: CanvasRenderingContext2D | null, showColor: string) {
         if (!context) {
             return;
         }
         context.save();
+        context.translate(this.x, this.y);
         context.beginPath();
         context.strokeStyle = showColor;
-        context.arc(this.x, this.y, this.radius, 0, Math.PI * 2, false);
+        this.points.forEach((p) => context.lineTo(p.x, p.y));
+        context.closePath();
         context.stroke();
         context.restore();
     }
@@ -34,6 +46,7 @@ export default class Background extends BaseCanvas {
         showColor: '#fff',
         minSize: 40
     };
+    private elements: Element[] = [];
     constructor(width: number, height: number) {
         super();
         this.initRect(width, height);
@@ -41,6 +54,17 @@ export default class Background extends BaseCanvas {
         this.initData();
     }
     initData() {
+        this.elements = [];
+        const radius = this.width / 30;
+        const margin = 2 * radius;
+        const xStep = 2 * radius * Math.cos(Math.PI / 6);
+        const yStep = 1.5 * radius
+        for (let y = 0; -margin + y * yStep < this.height + margin; y++) {
+            for (let x = 0; -margin + x * xStep < this.width + margin; x++) {
+                const startX = y % 2 == 0 ? -margin : -margin + radius * Math.cos(Math.PI / 6);
+                this.elements.push(new Element(startX + x * xStep, -margin + y * yStep, radius));
+            }
+        }
     }
     draw() {
         if (!this.context) {
@@ -49,8 +73,7 @@ export default class Background extends BaseCanvas {
         this.clear(Background.option.backgroundColor);
 
         this.context.save();
-        this.context.fillStyle = Background.option.showColor;
-        this.context.fillRect(200, 200, 400, 340);
+        this.elements.forEach((item) => item.draw(this.context, Background.option.showColor));
         this.context.restore();
         this.drawMark();
     }
