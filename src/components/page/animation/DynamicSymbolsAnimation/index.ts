88c38999ca3623b1d95c6e2849610fdcb771e853
ShenJinXiang <sjx-sword@163.com>
2024-08-22 17:22:20 +0800

    设置背景

--- src/components/page/animation/DynamicSymbolsAnimation/index.ts
+++ src/components/page/animation/DynamicSymbolsAnimation/index.ts
@@ -18,9 +18,29 @@ export default class DynamicSymbolsAnimation extends Animate {
     }
 
     this.context.save();
-    this.context.fillStyle = 'red';
-    this.context.fillRect(100, 200, 200, 120);
+    this.drawBackground();
     this.context.restore();
+  }
+
+  private drawBackground() {
+    if (!this.context) {
+      return;
+    }
+    const grad = this.context.createLinearGradient(0, this.height, this.width, 0);
+    grad.addColorStop(0, '#d16ba5');
+    grad.addColorStop(1 / 11, '#c777b9');
+    grad.addColorStop(2 / 11, '#ba83ca');
+    grad.addColorStop(3 / 11, '#aa8fd8');
+    grad.addColorStop(4 / 11, '#9a9ae1');
+    grad.addColorStop(5 / 11, '#8aa7ec');
+    grad.addColorStop(6 / 11, '#79b3f4');
+    grad.addColorStop(7 / 11, '#69bff8');
+    grad.addColorStop(8 / 11, '#52cffe');
+    grad.addColorStop(9 / 11, '#41dfff');
+    grad.addColorStop(10 / 11, '#46eefa');
+    grad.addColorStop(1, '#5ffbf1');
+    this.context.fillStyle = grad;
+    this.context.fillRect(0, 0, this.width, this.height);
   }
 
   public setRect(width: number, height: number) {
