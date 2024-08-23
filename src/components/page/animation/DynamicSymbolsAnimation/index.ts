bfa2cbc200b84689360d49e272b3576e258482f8
ShenJinXiang <sjx-sword@163.com>
2024-08-23 17:32:56 +0800

    绘制轨迹

--- src/components/page/animation/DynamicSymbolsAnimation/index.ts
+++ src/components/page/animation/DynamicSymbolsAnimation/index.ts
@@ -1,6 +1,23 @@
 import Animate from "@/lib/Animate";
 
+interface IOption {
+  showColor: string;
+  xRatio: number;
+  yRatio: number;
+}
+
+const OPTION: IOption = {
+  showColor: '#fff',
+  xRatio: 0.7,
+  yRatio: 5
+};
 export default class DynamicSymbolsAnimation extends Animate {
+
+  private endpointx: number = 0;
+  private endpointy: number = 0;
+  private contropointx: number = 0;
+  private contropointy: number = 0;
+  private lineWidth: number = 1;
 
   constructor(width: number, height: number) {
     super();
@@ -9,7 +26,12 @@ export default class DynamicSymbolsAnimation extends Animate {
   }
 
   initData() {
-
+    this.endpointx = this.width * 0.07;
+    this.endpointy = this.endpointx * OPTION.xRatio;
+    this.contropointx = this.endpointx * OPTION.yRatio;
+    this.contropointy = this.endpointy * OPTION.yRatio;
+    this.lineWidth = this.width * 0.005;
+    this.lineWidth = this.lineWidth < 2 ? this.lineWidth = 2 : this.lineWidth;
   }
 
   draw() {
@@ -19,6 +41,31 @@ export default class DynamicSymbolsAnimation extends Animate {
 
     this.context.save();
     this.drawBackground();
+    this.drawBaseline();
+    this.context.restore();
+  }
+
+  drawBaseline() {
+    if (!this.context) {
+      return;
+    }
+    this.context.save();
+    this.context.translate(this.width / 2, this.height / 2);
+    for (let i = 0; i < 2; i++) {
+      this.context.save();
+      if (i === 1) {
+        this.context.rotate(Math.PI);
+      }
+      this.context.beginPath();
+      this.context.strokeStyle = OPTION.showColor;
+      this.context.lineWidth = this.lineWidth;
+      this.context.moveTo(0, 0);
+      this.context.lineTo(this.endpointx, this.endpointy);
+      this.context.bezierCurveTo(this.contropointx, this.contropointy, this.contropointx, -this.contropointy, this.endpointx, -this.endpointy);
+      this.context.lineTo(0, 0);
+      this.context.stroke();
+      this.context.restore();
+    }
     this.context.restore();
   }
 
